410a9a81213cf73b2d9304a0d6ef3ee5df0628c4
carloderamo <carlo.deramo@gmail.com>
2017-04-21 17:57:20 +0200

    examples td updated

--- examples/td.py
+++ examples/td.py
@@ -1,38 +1,50 @@
-from PyPi.agent import Agent
+import numpy as np
+from joblib import Parallel, delayed
 
+from PyPi.agent import Agent
 from PyPi.utils.loader import *
 from PyPi.utils.parameters import Parameter
 
 
-mdp, policy, approximator, config = load_experiment()
+def experiment():
+    np.random.seed()
 
-# Agent
-import numpy as np
-#discrete_actions = np.linspace(
-#    mdp.action_space.low, mdp.action_space.high, 5).reshape(-1, 1)
-discrete_actions = mdp.action_space.values
-agent = Agent(approximator, policy, discrete_actions=discrete_actions)
+    mdp, policy, approximator, config = load_experiment()
 
-# Algorithm
-from keras.callbacks import EarlyStopping
-config['algorithm']['params']['fit_params']['callbacks'] =\
-    [EarlyStopping('loss', patience=20, min_delta=1e-4)]
-alg = get_algorithm(config['algorithm']['name'],
-                    agent,
-                    mdp,
-                    **config['algorithm']['params'])
+    # Agent
+    #discrete_actions = np.linspace(
+    #    mdp.action_space.low, mdp.action_space.high, 5).reshape(-1, 1)
+    discrete_actions = mdp.action_space.values
+    agent = Agent(approximator, policy, discrete_actions=discrete_actions)
 
-# Train
-alg.learn(n_iterations=1, how_many=1000, n_fit_steps=20,
-          iterate_over='episodes')
+    # Algorithm
+    #from keras.callbacks import EarlyStopping
+    #config['algorithm']['params']['fit_params']['callbacks'] =\
+    #    [EarlyStopping('loss', patience=20, min_delta=1e-4)]
+    alg = get_algorithm(config['algorithm']['name'],
+                        agent,
+                        mdp,
+                        **config['algorithm']['params'])
 
-# Test
-agent.policy.set_epsilon(Parameter(0))
+    # Train
+    alg.learn(n_iterations=1, how_many=1000, n_fit_steps=20,
+              iterate_over='episodes')
 
-initial_states = np.zeros((289, 2))
-cont = 0
-for i in range(-8, 9):
-    for j in range(-8, 9):
-        initial_states[cont, :] = [0.125 * i, 0.375 * j]
-        cont += 1
-print(np.mean(alg.evaluate(initial_states, render=False)))
+    # Test
+    agent.policy.set_epsilon(Parameter(0))
+
+    initial_states = np.zeros((289, 2))
+    cont = 0
+    for i in range(-8, 9):
+        for j in range(-8, 9):
+            initial_states[cont, :] = [0.125 * i, 0.375 * j]
+            cont += 1
+
+    return np.mean(alg.evaluate(initial_states, render=False))
+
+
+if __name__ == '__main__':
+    n_experiment = 3
+
+    Js = Parallel(n_jobs=-1)(delayed(experiment)() for _ in range(n_experiment))
+    print(Js)
