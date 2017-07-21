4c7274b9d66259ff24adf01680a7acda4343b1c1
carloderamo <carlo.deramo@gmail.com>
2017-07-21 21:24:04 +0200

    removed pendulum (will be re-added)

--- PyPi/environments/__init__.py
+++ PyPi/environments/__init__.py
@@ -4,8 +4,7 @@ from .car_on_hill import CarOnHill
 from generators.simple_chain import generate_simple_chain
 from .grid_world import GridWorld, GridWorldVanHasselt, GridWorldGenerator
 from .finite_mdp import FiniteMDP
-from .pendulum import Pendulum
 
 __all__ = ['Atari', 'CarOnHill', 'Environment', 'FiniteMDP', 'GridWorld',
            'generate_simple_chain', 'GridWorldVanHasselt',
-           'GridWorldGenerator', 'Pendulum']
+           'GridWorldGenerator']
