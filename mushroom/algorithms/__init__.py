cc2d5e9925e5800abce9baf9b977559ff5fd7890
carloderamo <carloderamo@gmail.com>
2017-10-19 18:00:39 +0200

    removed deep fqi in init

--- mushroom/algorithms/__init__.py
+++ mushroom/algorithms/__init__.py
@@ -1,5 +1,5 @@
 from mushroom.algorithms.value.dqn import DQN, DoubleDQN
 
-__all__ = ['Algorithm', 'BatchTD', 'FQI', 'DeepFQI', 'DQN', 'DoubleDQN',
+__all__ = ['Algorithm', 'BatchTD', 'FQI', 'DQN', 'DoubleDQN',
            'DoubleFQI', 'WeightedFQI', 'TD', 'QLearning', 'DoubleQLearning',
            'WeightedQLearning', 'SARSA']
