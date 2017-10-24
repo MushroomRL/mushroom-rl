a59453ebc83003b83f86d59831c5ad918d4679ad
carloderamo <carloderamo@gmail.com>
2017-10-24 18:28:14 +0200

    atari_dqn script updated

--- examples/atari_dqn/atari_dqn.py
+++ examples/atari_dqn/atari_dqn.py
@@ -5,7 +5,8 @@ import os
 import numpy as np
 
 from convnet import ConvNet
-from mushroom.algorithms.value.dqn import DQN, DoubleDQN, WeightedDQN
+from mushroom.algorithms.value.dqn import AveragedDQN, DQN, DoubleDQN,\
+    WeightedDQN
 from mushroom.approximators import Regressor
 from mushroom.core.core import Core
 from mushroom.environments import *
