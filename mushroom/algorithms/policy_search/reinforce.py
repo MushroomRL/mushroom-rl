Fixed gradient of multivariate gaussian policy
- changed weights_shape in weights_size
- changed interface to multivariate gaussian policy
- started implementation of multivariate diagonal gaussian policy, i.e.
  a gaussian policy with diagonal learnable standard deviation.
- fixed tests/policy/gaussian
- fixed ship steering example

mushroom/algorithms/policy_search/reinforce.py
@@ -46,4 +46,4 @@ class REINFORCE(PolicyGradient):
         self.baseline_den.append(squared_sum_d_log_pi)
 
     def _init_update(self):
-        self.sum_d_log_pi = np.zeros(self.policy.weights_shape)
+        self.sum_d_log_pi = np.zeros(self.policy.weights_size)

mushroom/approximators/parametric/linear.py
@@ -2,33 +2,44 @@ import numpy as np
 
 
 class LinearApproximator:
-    def __init__(self, params=None, params_shape=None):
+    def __init__(self, params=None, input_dim=None, output_dim=1):
         if params is not None:
-            self._w = params
-        elif params_shape is not None:
-            self._w = np.zeros(params_shape)
+            self._w = params.reshape((output_dim, -1))
+        elif input_dim is not None:
+            self._w = np.zeros((output_dim, input_dim))
         else:
             raise ValueError('You should specify the initial parameter vector'
-                             'or the parameter shape')
+                             'or the input dimension')
 
     def fit(self, x, y, **fit_params):
-        self._w = np.solve(x, y)
+        self._w = np.solve(x, y).T
 
     def predict(self, x, **predict_params):
-        return np.dot(x, self._w)
+        return np.dot(x, self._w.T)
 
     def get_weights(self):
-        return self._w
+        return self._w.flatten()
 
     def set_weights(self, w):
-        self._w = w
+        self._w = w.reshape(self._w.shape)
 
     @property
-    def weights_shape(self):
-        return self._w.shape
+    def weights_size(self):
+        return self._w.size
 
     def diff(self, x):
-        if len(self._w.shape) == 1 or self._w.shape[1] == 1:
+        if len(self._w.shape) == 1 or self._w.shape[0] == 1:
             return x
         else:
-            return np.array([x] * self._w.shape[1])
+            n_phi = self._w.shape[1]
+            n_outs = self._w.shape[0]
+
+            shape = (n_phi*n_outs, n_outs)
+            df = np.zeros(shape)
+
+            start = 0
+            for i in xrange(n_outs):
+                end = start + n_phi
+                df[start:end, i] = x
+                start = end
+            return df

mushroom/policy/gaussian_policy.py
@@ -13,16 +13,18 @@ class GaussianPolicy:
         self._approximator = mu
         self._sigma = sigma
 
-    def __call__(self, *args):
-        if len(args) == 1:
-            return self._sample_action(args[0])
-        elif len(args) == 2:
-            return self._compute_prob(args[0], args[1])
+    def __call__(self, state, action):
+        mu, sigma = self._compute_gaussian(state, False)
 
-        raise ValueError('args must be state, or state and action')
+        return norm.pdf(action[0], mu[0], sigma)
+
+    def draw_action(self, state):
+        mu, sigma = self._compute_gaussian(state)
+
+        return np.random.normal(mu, sigma)
 
     def diff(self, state, action):
-        return self._compute_prob(state, action) * self.diff_log(state, action)
+        return self(state, action) * self.diff_log(state, action)
 
     def diff_log(self, state, action):
         mu, sigma = self._compute_gaussian(state, False)
@@ -31,15 +33,12 @@ class GaussianPolicy:
 
         g = g_mu.dot(delta) / sigma**2
 
-        return np.expand_dims(g, axis=1)
+        return g
 
     def set_sigma(self, sigma):
         assert isinstance(sigma, Parameter)
 
         self._sigma = sigma
-
-    def update(self, *idx):
-        self._sigma.update(*idx)
 
     def set_weights(self, weights):
         self._approximator.set_weights(weights)
@@ -48,28 +47,17 @@ class GaussianPolicy:
         return self._approximator.get_weights()
 
     @property
-    def weights_shape(self):
-        return self._approximator.weights_shape
+    def weights_size(self):
+        return self._approximator.weights_size
 
     def _compute_gaussian(self, state, update=True):
         if update:
             sigma = self._sigma(state)
         else:
             sigma = self._sigma.get_value(state)
-        mu = np.reshape(self._approximator.predict(np.expand_dims(state,
-                                                                  axis=0)), -1)
+        mu = np.reshape(self._approximator.predict(np.expand_dims(state, axis=0)), -1)
 
         return mu, sigma
-
-    def _sample_action(self, state):
-        mu, sigma = self._compute_gaussian(state)
-
-        return np.random.normal(mu, sigma)
-
-    def _compute_prob(self, state, action):
-        mu, sigma = self._compute_gaussian(state, False)
-
-        return norm.pdf(action[0], mu[0], sigma)
 
     def __str__(self):
         return self.__name__
@@ -83,37 +71,37 @@ class MultivariateGaussianPolicy:
         self._inv_sigma = np.linalg.inv(sigma)
         self._sigma = sigma
 
-    def __call__(self, *args):
-        if len(args) == 1:
-            return self._sample_action(args[0])
-        elif len(args) == 2:
-            return self._compute_prob(args[0], args[1])
+    def __call__(self, state, action):
+        mu, sigma, _ = self._compute_multivariate_gaussian(state)
 
-        raise ValueError('args must be state, or state and action')
+        return multivariate_normal.pdf(action, mu, sigma)
+
+    def draw_action(self, state):
+        mu, sigma, _ = self._compute_multivariate_gaussian(state)
+
+        return np.random.multivariate_normal(mu, sigma)
 
     def diff(self, state, action):
-        return self._compute_prob(state, action) * self.diff_log(state, action)
+        return self(state, action) * self.diff_log(state, action)
 
     def diff_log(self, state, action):
 
         mu, _, inv_sigma = self._compute_multivariate_gaussian(state)
 
         delta = action - mu
-        g_mu = np.expand_dims(self._approximator.diff(state), axis=1)
 
-        g = 0.5 * g_mu.dot(inv_sigma + inv_sigma.T).dot(delta)
+        g_mu = self._approximator.diff(state)
 
-        if len(g.shape) == 1:
-            return np.expand_dims(g, axis=1)
-        else:
-            return g
+        if len(g_mu.shape) == 1:
+            g_mu = np.expand_dims(g_mu, axis=1)
+
+        g = 0.5 * g_mu.dot(inv_sigma + inv_sigma.T).dot(delta.T)
+
+        return g
 
     def set_sigma(self, sigma):
         self._sigma = sigma
         self._inv_sigma = np.linalg.inv(sigma)
-
-    def update(self, *idx):
-        pass
 
     def set_weights(self, weights):
         self._approximator.set_weights(weights)
@@ -122,24 +110,74 @@ class MultivariateGaussianPolicy:
         return self._approximator.get_weights()
 
     @property
-    def weights_shape(self):
-        return self._approximator.weights_shape
+    def weights_size(self):
+        return self._approximator.weights_size
 
     def _compute_multivariate_gaussian(self, state):
-        mu = np.reshape(self._approximator.predict(np.expand_dims(state,
-                                                                  axis=0)), -1)
+        mu = np.reshape(self._approximator.predict(np.expand_dims(state, axis=0)), -1)
 
         return mu, self._sigma, self._inv_sigma
 
-    def _sample_action(self, state):
+    def __str__(self):
+        return self.__name__
+
+
+class MultivariateDiagonalGaussianPolicy:
+    def __init__(self, mu, sigma):
+        self.__name__ = 'MultivariateDiagonalGaussianPolicy'
+
+        self._approximator = mu
+        self._sigma = sigma
+
+    def __call__(self, state, action):
+        mu, sigma, _ = self._compute_multivariate_gaussian(state)
+
+        return multivariate_normal.pdf(action, mu, sigma)
+
+    def draw_action(self, state):
         mu, sigma, _ = self._compute_multivariate_gaussian(state)
 
         return np.random.multivariate_normal(mu, sigma)
 
-    def _compute_prob(self, state, action):
-        mu, sigma, _ = self._compute_multivariate_gaussian(state)
+    def diff(self, state, action):
+        return self(state, action) * self.diff_log(state, action)
 
-        return multivariate_normal.pdf(action, mu, sigma)
+    def diff_log(self, state, action):
+
+        mu, _, inv_sigma = self._compute_multivariate_gaussian(state)
+
+        delta = action - mu
+
+        g_mu = self._approximator.diff(state)
+
+        if len(g_mu.shape) == 1:
+            g_mu = np.expand_dims(g_mu, axis=1)
+
+        g = 0.5 * g_mu.dot(inv_sigma + inv_sigma.T).dot(delta.T)
+
+        g_sigma = -1.0/self._sigma + delta**2/self._sigma**3
+
+        return np.concatenate(g, g_sigma)
+
+    def set_sigma(self, sigma):
+        self._sigma = sigma
+
+    def set_weights(self, weights):
+        self._approximator.set_weights(weights[0:self._approximator.weights_size])
+        self._sigma = weights[self._approximator.weights_size:]
+
+    def get_weights(self):
+        return np.concatenate(self._approximator.get_weights(), self._sigma)
+
+    @property
+    def weights_size(self):
+        return self._approximator.weights_size + self._sigma.size
+
+    def _compute_multivariate_gaussian(self, state):
+        mu = np.reshape(self._approximator.predict(np.expand_dims(state, axis=0)), -1)
+
+        sigma2 = self._sigma**2
+        return mu, np.diag(sigma2), np.diag(1.0/sigma2)
 
     def __str__(self):
         return self.__name__

tests/policy/gaussians.py
@@ -5,7 +5,7 @@ from mushroom.approximators.regressor import Regressor
 from mushroom.utils.parameters import Parameter
 
 input_shape = (2,)
-approximator_params = dict(params=np.array([[1.0], [0.5]]))
+approximator_params = dict(params=np.array([1.0, 0.5]))
 approximator = Regressor(LinearApproximator, input_shape=input_shape,
                              output_shape=(1,),
                              params=approximator_params)
@@ -32,7 +32,7 @@ print 'p(action|state): ', policy_2(state, action)
 print 'd log(p(action|state)): ', policy_2.diff_log(state, action)
 
 
-approximator_params_m = dict(params=np.array([[1.0, 2.0], [0.5, 1.0]]))
+approximator_params_m = dict(params=np.array([1.0, 0.0, 0.0, 1.0]), output_dim=2)
 approximator_m = Regressor(LinearApproximator, input_shape=input_shape,
                              output_shape=(2,),
                              params=approximator_params_m)
@@ -40,15 +40,12 @@ approximator_m = Regressor(LinearApproximator, input_shape=input_shape,
 sigma_m = np.eye(2, 2)
 policy_m = MultivariateGaussianPolicy(mu=approximator_m, sigma=sigma_m)
 
-
-state = np.array([1.3, 2.4])
-action = np.array([2.4, 4.5])
+state = np.array([-1.2731,  -2.4746])
+action = np.array([-2.2535,  -2.7151])
 
 print 'state: ', state
 print 'action: ', action
 
 print 'Multivariate gaussian, multiple variable'
 print 'p(action|state): ', policy_m(state, action)
-print 'd log(p(action|state)): ', policy_m.diff_log(state, action)
-
-print 'end'
+print 'd p(action|state): ', policy_m.diff(state, action)