4ac7cf625b667ef1ac13b076561e299178cc977a
carloderamo <carloderamo@gmail.com>
2018-03-23 18:09:39 +0100

    fix in policy_search init

--- mushroom/algorithms/policy_search/__init__.py
+++ mushroom/algorithms/policy_search/__init__.py
@@ -1,8 +1,8 @@
-from .black_box_optimization import RWR, PGPE, REPS
-from .enac import eNAC
-from .gpomdp import GPOMDP
 from .policy_gradient import PolicyGradient
 from .reinforce import REINFORCE
+from .enac import eNAC
+from .gpomdp import GPOMDP
+from .black_box_optimization import RWR, PGPE, REPS
 
 
 __all__ = ['PolicyGradient', 'REINFORCE', 'GPOMDP', 'eNAC', 'RWR', 'PGPE',
