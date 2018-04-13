0ba6a6b2825b68fff48634de6258c292ab298d82
boris-il-forte <boris.ilpossente@hotmail.it>
2018-04-13 11:17:09 +0200

    Fixed mountain car with SARSA

--- examples/mountain_car_sarsa.py
+++ examples/mountain_car_sarsa.py
@@ -30,31 +30,31 @@ def experiment(alpha):
     pi = EpsGreedy(epsilon=epsilon)
 
     # Agent
-    learning_rate = Parameter(alpha)
-    tilings = Tiles.generate(5, [5, 5],
+    n_tilings = 10
+    tilings = Tiles.generate(n_tilings, [10, 10],
                              mdp.info.observation_space.low,
                              mdp.info.observation_space.high)
     features = Features(tilings=tilings)
+
+    learning_rate = Parameter(alpha/n_tilings)
 
     approximator_params = dict(input_shape=(features.size,),
                                output_shape=(mdp.info.action_space.n,),
                                n_actions=mdp.info.action_space.n)
     algorithm_params = {'learning_rate': learning_rate,
-                        'lambda': .9}
+                        'lambda_coeff': .9}
 
     agent = TrueOnlineSARSALambda(pi, mdp.info,
                                   approximator_params=approximator_params,
                                   features=features, **algorithm_params)
 
     # Algorithm
-    collect_dataset = CollectDataset()
-    callbacks = [collect_dataset]
-    core = Core(agent, mdp, callbacks=callbacks)
+    core = Core(agent, mdp)
 
     # Train
-    core.learn(n_episodes=20, n_steps_per_fit=1, render=0)
+    core.learn(n_episodes=40, n_steps_per_fit=1, render=False)
+    dataset = core.evaluate(n_episodes=1, render=True)
 
-    dataset = collect_dataset.get()
     return np.mean(compute_J(dataset, 1.))
 
 
