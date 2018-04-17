d7b3f54f6ea5a341bcd84d18610535bfcf919421
carloderamo <carloderamo@gmail.com>
2018-04-17 18:40:38 +0200

    support for NoFrameskip added in atari

--- mushroom/environments/atari.py
+++ mushroom/environments/atari.py
@@ -6,6 +6,39 @@ from mushroom.environments import Environment, MDPInfo
 from mushroom.utils.spaces import *
 
 
+class MaxAndSkip(gym.Wrapper):
+    def __init__(self, env, skip, max_pooling=True):
+        gym.Wrapper.__init__(self, env)
+        self._obs_buffer = np.zeros((2,) + env.observation_space.shape,
+                                    dtype=np.uint8)
+        self._skip = skip
+        self._max_pooling = max_pooling
+
+    def reset(self):
+        return self.env.reset()
+
+    def step(self, action):
+        total_reward = 0.
+        for i in range(self._skip):
+            obs, reward, absorbing, info = self.env.step(action)
+            if i == self._skip - 2:
+                self._obs_buffer[0] = obs
+            if i == self._skip - 1:
+                self._obs_buffer[1] = obs
+            total_reward += reward
+            if absorbing:
+                break
+        if self._max_pooling:
+            frame = self._obs_buffer.max(axis=0)
+        else:
+            frame = self._obs_buffer.mean(axis=0)
+
+        return frame, total_reward, absorbing, info
+
+    def reset(self, **kwargs):
+        return self.env.reset(**kwargs)
+
+
 class Atari(Environment):
     """
     The Atari environment as presented in:
@@ -13,25 +46,32 @@ class Atari(Environment):
     2015.
 
     """
-    def __init__(self, name, width=84, height=84, ends_at_life=False):
+    def __init__(self, name, width=84, height=84, ends_at_life=False,
+                 max_pooling=True):
         """
         Constructor.
 
         Args:
-             name (str): id name of the Atari game in Gym;
-             width (int, 84): width of the screen;
-             height (int, 84): height of the screen;
-             ends_at_life (bool, False): whether the episode ends when a life is
-                lost or not.
+            name (str): id name of the Atari game in Gym;
+            width (int, 84): width of the screen;
+            height (int, 84): height of the screen;
+            ends_at_life (bool, False): whether the episode ends when a life is
+               lost or not;
+            max_pooling (bool, True): whether to do max-pooling or
+                average-pooling of the last two frames when using NoFrameskip.
 
         """
         # MPD creation
-        self.env = gym.make(name)
+        if 'NoFrameskip' in name:
+            skip = 3 if 'SpaceInvaders' in name else 4
+            self.env = MaxAndSkip(gym.make(name), skip, max_pooling)
+        else:
+            self.env = gym.make(name)
 
         # MDP parameters
         self.img_size = (width, height)
         self._episode_ends_at_life = ends_at_life
-        self._max_lives = self.env.env.ale.lives()
+        self._max_lives = self.env.env.env.ale.lives()
         self._lives = self._max_lives
         self._force_fire = self.env.unwrapped.get_action_meanings()[1] == 'FIRE'
 
