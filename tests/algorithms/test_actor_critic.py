e98df967b1f6adea7e7aae2f5999d7e5548bb870
carloderamo <carloderamo@gmail.com>
2018-06-28 15:30:20 +0200

    actor critic test added

--- tests/algorithms/test_actor_critic.py
+++ tests/algorithms/test_actor_critic.py
@@ -40,7 +40,7 @@ agent_test = COPDAC_Q(policy, mu, mdp.info,
 
 core = Core(agent_test, mdp)
 
-s = np.arange(10)
+s = np.array([np.arange(10), np.arange(10)]).T
 a = np.arange(10)
 r = np.arange(10)
 ss = s + 5
@@ -48,9 +48,9 @@ ab = np.array([0, 0, 0, 0, 1, 0, 0, 0, 1, 0])
 last = np.array([0, 0, 0, 0, 1, 0, 0, 0, 1, 0])
 
 dataset = list()
-for i in range(s.size):
-    dataset.append([np.array([s[i]]), np.array([a[i]]), r[i],
-                    np.array([ss[i]]), np.array([ab[i]]), np.array([last[i]])])
+for i in range(s.shape[0]):
+    dataset.append([s[i], np.array([a[i]]), r[i], ss[i], np.array([ab[i]]),
+                    np.array([last[i]])])
 
 
 def test_copdac_q():
@@ -58,16 +58,50 @@ def test_copdac_q():
                      value_function_features=phi, policy_features=phi)
     agent.fit(dataset)
 
-    np.save('w.npy', agent.policy.get_weights())
+    w_1 = .05
+    w_2 = .1
 
-    w = np.array([-.07071067, 2.07071068])
+    w = agent._V.get_weights()
 
-    assert np.allclose(w, agent.policy.get_weights())
+    np.save('w.npy', w)
+
+    assert np.allclose(w_1, w[66])
+    assert np.allclose(w_2, w[78])
 
 
 def test_sac():
-    pass
+    tilings_v = tilings + Tiles.generate(1, [1, 1],
+                                         mdp.info.observation_space.low,
+                                         mdp.info.observation_space.high + 1e-3)
+    psi = Features(tilings=tilings_v)
+    agent = SAC(policy, mdp.info, alpha_theta, alpha_v, lambda_par=.5,
+                value_function_features=psi, policy_features=phi)
+    agent.fit(dataset)
+
+    w_1 = .09370429
+    w_2 = .28141735
+
+    w = agent._V.get_weights()
+
+    assert np.allclose(w_1, w[65])
+    assert np.allclose(w_2, w[78])
 
 
 def test_sac_avg():
-    pass
+    alpha_r = Parameter(.0001)
+    tilings_v = tilings + Tiles.generate(1, [1, 1],
+                                         mdp.info.observation_space.low,
+                                         mdp.info.observation_space.high + 1e-3)
+    psi = Features(tilings=tilings_v)
+    agent = SAC_AVG(policy, mdp.info, alpha_theta, alpha_v, alpha_r,
+                    lambda_par=.5, value_function_features=psi,
+                    policy_features=phi)
+    agent.fit(dataset)
+
+    w_1 = .09645764
+    w_2 = .28583057
+
+    w = agent._V.get_weights()
+
+    assert np.allclose(w_1, w[65])
+    assert np.allclose(w_2, w[78])
