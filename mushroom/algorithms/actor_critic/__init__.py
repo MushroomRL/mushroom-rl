a99ecb6295b6813c73f00a4fd04a7dd69829b1fa
boris-il-forte <boris.ilpossente@hotmail.it>
2019-08-15 14:25:52 +0200

    Merged all reparametrization AC in a single file
    - All reparametrization AC are in a single file

--- mushroom/algorithms/actor_critic/__init__.py
+++ mushroom/algorithms/actor_critic/__init__.py
@@ -1,7 +1,5 @@
 from .dpg import COPDAC_Q
-from .reparametrization_ac import ReparametrizationAC
-from .ddpg import DDPG, TD3
-from .sac import SAC
+from .reparametrization_ac import ReparametrizationAC, DDPG, TD3, SAC
 from .stochastic_actor_critic import StochasticAC, StochasticAC_AVG
 from .trust_region import TRPO, PPO
 
