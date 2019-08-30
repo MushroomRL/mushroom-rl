82ffdd9a661407a3788ffa0fcb55a2d35c7099aa
boris-il-forte <boris.ilpossente@hotmail.it>
2019-08-30 14:02:15 +0200

    Work on classic actor critic algorithms
    - Removed specific files, created single file with every classic (non
    deep) algorithm
    
    - Added docstring to COPDAC-Q constructor

--- mushroom/algorithms/actor_critic/__init__.py
+++ mushroom/algorithms/actor_critic/__init__.py
@@ -1,6 +1,6 @@
-from .dpg import COPDAC_Q
+from .classic_actor_critic import StochasticAC, StochasticAC_AVG, COPDAC_Q
 from .reparametrization_ac import ReparametrizationAC, DDPG, TD3, SAC
-from .stochastic_actor_critic import StochasticAC, StochasticAC_AVG
 from .trust_region import TRPO, PPO
 
-__all__ = ['COPDAC_Q', 'StochasticAC', 'StochasticAC_AVG', 'ReparametrizationAC', 'DDPG', 'TD3', 'SAC', 'TRPO', 'PPO']
+__all__ = ['COPDAC_Q', 'StochasticAC', 'StochasticAC_AVG',
+           'ReparametrizationAC', 'DDPG', 'TD3', 'SAC', 'TRPO', 'PPO']
