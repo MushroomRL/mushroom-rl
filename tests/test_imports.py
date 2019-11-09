ba875ddc9ca9d94795f0bb3ed6d65484b3d5edbb
boris-il-forte <boris.ilpossente@hotmail.it>
2019-11-09 15:34:33 +0100

    Added imports test

---
+++ tests/test_imports.py
@@ -0,0 +1,41 @@
+
+
+def test_imports():
+    import mushroom
+
+    import mushroom.algorithms
+    import mushroom.algorithms.actor_critic
+    import mushroom.algorithms.actor_critic.classic_actor_critic
+    import mushroom.algorithms.actor_critic.deep_actor_critic
+    import mushroom.algorithms.policy_search
+    import mushroom.algorithms.policy_search.black_box_optimization
+    import mushroom.algorithms.policy_search.policy_gradient
+    import mushroom.algorithms.value
+    import mushroom.algorithms.value.batch_td
+    import mushroom.algorithms.value.td
+    import mushroom.algorithms.value.dqn
+
+    import mushroom.approximators
+    import mushroom.approximators._implementations
+    import mushroom.approximators.parametric
+
+    import mushroom.core
+
+    import mushroom.distributions
+
+    import mushroom.environments
+    import mushroom.environments.generators
+    import mushroom.environments.mujoco_envs
+
+    import mushroom.features
+    import mushroom.features._implementations
+    import mushroom.features.basis
+    import mushroom.features.tensors
+    import mushroom.features.tiles
+
+    import mushroom.policy
+
+    import mushroom.solvers
+
+    import mushroom.utils
+
