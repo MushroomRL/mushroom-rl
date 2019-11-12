ef2c90d2687776eb4b349d955c4d9831426912c7
boris-il-forte <boris.ilpossente@hotmail.it>
2019-11-12 09:46:34 +0100

    Added test for distribution interface

---
+++ tests/distributions/test_distribution_interface.py
@@ -0,0 +1,31 @@
+from mushroom.distributions import Distribution
+
+
+def abstract_method_tester(f, *args):
+    try:
+        f(*args)
+    except NotImplementedError:
+        pass
+    else:
+        assert False
+
+
+def test_distribution_interface():
+    tmp = Distribution()
+
+    abstract_method_tester(tmp.sample)
+    abstract_method_tester(tmp.log_pdf, None)
+    abstract_method_tester(tmp.__call__, None)
+    abstract_method_tester(tmp.mle, None)
+    abstract_method_tester(tmp.diff_log, None)
+    abstract_method_tester(tmp.diff, None)
+
+    abstract_method_tester(tmp.get_parameters)
+    abstract_method_tester(tmp.set_parameters, None)
+
+    try:
+        tmp.parameters_size
+    except NotImplementedError:
+        pass
+    else:
+        assert False
