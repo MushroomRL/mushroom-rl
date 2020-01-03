fb5b28dfa6b25a9cd8aa3a6f2ed07c5c94fa8cca
Boris_il_forte <davide.tateo90@gmail.com>
2020-01-03 17:46:36 +0100

    Cleanup setup.py
    - removed comments
    - added license name

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
-"""A setuptools based setup module.
-
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
-"""
-
-# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
-# To use a consistent encoding
 from codecs import open
 from os import path
 
@@ -16,13 +7,11 @@ from mushroom import __version__
 here = path.abspath(path.dirname(__file__))
 name = 'mushroom'
 
-# Get the long description from the README file
 requires_list = []
 with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
     for line in f:
         requires_list.append(str(line))
 
-# specific dependencies for modules
 extras = {
     'gym': ['gym'],
     'atari': ['atari_py~=0.2.0', 'Pillow', 'opencv-python'],
@@ -31,7 +20,6 @@ extras = {
     'mujoco': ['mujoco_py']
 }
 
-# Meta dependency groups.
 all_deps = []
 for group_name in extras:
     if group_name != 'mujoco':
@@ -40,44 +28,15 @@ extras['all'] = all_deps
 
 setup(
     name=name,
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
     version=__version__,
-
     description='A Python toolkit for Reinforcement Learning experiments.',
-
-    # The project's main homepage.
-    url='https://github.com/AIRLab-POLIMI/' + name,
-
-    # Author details
+    url='https://github.com/AIRLab-POLIMI/mushroom',
     author="Carlo D'Eramo",
     author_email='carlo.deramo@gmail.com',
-
-    # Choose your license
-    license='',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
+    license='MIT',
     packages=[package for package in find_packages()
               if package.startswith(name)],
-
     zip_safe=False,
-
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
     install_requires=requires_list,
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,tests,all]
     extras_require=extras
 )
