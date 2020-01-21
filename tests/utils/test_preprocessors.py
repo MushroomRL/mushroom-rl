Small improvements to normalization + plotting callbacks -Added mushroom_rl example with both normalization and plotting -Small fixes for consistency     - now plotting only receives mdp_info instead of full mdp     - state dictionary is implemented in a more similar way     - plotting state save file now correctly ignores plots that should not be saved     - small name fixes

tests/utils/test_preprocessors.py
@@ -17,7 +17,7 @@ from mushroom_rl.environments.cart_pole import CartPole
 from mushroom_rl.utils.preprocessors import NormalizationBoxedPreprocessor
 
 
-def test_normalizing_():
+def test_normalizing_preprocessor():
     np.random.seed(88)
 
     class Network(nn.Module):