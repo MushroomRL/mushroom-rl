b638480c30785d0a415701b100e45de380f8bda6
Boris_il_forte <boris.ilpossente@hotmail.it>
2020-03-23 23:56:16 +0100

    Created pybullet viewer
    - Created class, using pygame viewer

--- mushroom_rl/environments/pybullet.py
+++ mushroom_rl/environments/pybullet.py
@@ -5,6 +5,7 @@ import numpy as np
 from enum import Enum
 from mushroom_rl.environments import Environment, MDPInfo
 from mushroom_rl.utils.spaces import Box
+from mushroom_rl.utils.viewer import ImageViewer
 
 
 class PyBulletObservationType(Enum):
@@ -16,6 +17,43 @@ class PyBulletObservationType(Enum):
     __order__ = "JOINT_POS JOINT_VEL"
     JOINT_POS = 0
     JOINT_VEL = 1
+
+
+class PyBulletViewer(ImageViewer):
+    def __init__(self, size, dt, distance=4, origin=(0,0,1), angles=(0, -45, 60),
+                 fov=60, aspect=1, near_val=0.01, far_val=100):
+        self._size = size
+        self._distance = distance
+        self._origin = origin
+        self._angles = angles
+        self._fov = fov
+        self._aspect = aspect
+        self._near_val = near_val
+        self._far_val = far_val
+        super().__init__(size, dt)
+
+    def display(self):
+        img = self._get_image()
+        super().display(img)
+
+    def _get_image(self):
+        view_matrix = pybullet.computeViewMatrixFromYawPitchRoll(cameraTargetPosition=self._origin,
+                                                                 distance=self._distance,
+                                                                 roll=self._angles[0],
+                                                                 pitch=self._angles[1],
+                                                                 yaw=self._angles[2],
+                                                                 upAxisIndex=2)
+        proj_matrix = pybullet.computeProjectionMatrixFOV(fov=self._fov, aspect=self._aspect,
+                                                          nearVal=self._near_val, farVal=self._far_val)
+        (_, _, px, _, _) = pybullet.getCameraImage(width=self._size[0],
+                                                   height=self._size[1],
+                                                   viewMatrix=view_matrix,
+                                                   projectionMatrix=proj_matrix,
+                                                   renderer=pybullet.ER_BULLET_HARDWARE_OPENGL)
+
+        rgb_array = np.reshape(np.array(px), (self._size[0], self._size[1], -1))
+        rgb_array = rgb_array[:, :, :3]
+        return rgb_array
 
 
 class PyBullet(Environment):
@@ -63,7 +101,7 @@ class PyBullet(Environment):
         self._timestep = timestep
 
         # Create the simulation
-        pybullet.connect(pybullet.GUI)
+        pybullet.connect(pybullet.DIRECT)
         pybullet.setTimeStep(self._timestep)
         pybullet.setAdditionalSearchPath(pybullet_data.getDataPath())
 
@@ -131,7 +169,7 @@ class PyBullet(Environment):
         # the parent class
         mdp_info = MDPInfo(observation_space, action_space, gamma, horizon)
         super().__init__(mdp_info)
-
+        self._viewer = PyBulletViewer((500, 500), self._timestep*self.n_intermediate_steps)
         self._initial_state = pybullet.saveState()
 
     def seed(self, seed):
@@ -146,7 +184,7 @@ class PyBullet(Environment):
         return self._state
 
     def render(self):
-        time.sleep(self._timestep*self.n_intermediate_steps)
+        self._viewer.display()
 
     def stop(self):
         pass
