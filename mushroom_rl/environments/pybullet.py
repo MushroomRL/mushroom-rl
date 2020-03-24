b1b3ed119755f2396dacca7555aaffd4892abaab
Boris_il_forte <boris.ilpossente@hotmail.it>
2020-03-24 11:11:27 +0100

    Progress in pybullet simulator
    - added per joint control type
    - added custom load of objects
    - improved ball_rolling example

--- mushroom_rl/environments/pybullet.py
+++ mushroom_rl/environments/pybullet.py
@@ -60,17 +60,16 @@ class PyBullet(Environment):
     Class to create a Mushroom environment using the PyBullet simulator.
 
     """
-    def __init__(self, file_name, actuation_spec, control_mode, observation_spec, gamma,
+    def __init__(self, files, actuation_spec, control_mode, observation_spec, gamma,
                  horizon, timestep=1/240, n_intermediate_steps=1):
         """
         Constructor.
 
         Args:
-            file_name (string): The path to the urdf file with which the
-                environment should be created;
-            actuation_spec (list): A list specifying the names of the joints
-                which should be controllable by the agent. Can be left empty
-                when all actuators should be used;
+            files (list): Paths to the URDF files to load;
+            actuation_spec (list): A list of tuples specifying the names of the
+                joints which should be controllable by the agent and tehir control mode.
+                 Can be left empty when all actuators should be used in position control;
             observation_spec (list): A list containing the names of data that
                 should be made available to the agent as an observation and
                 their type (ObservationType). An entry in the list is given by:
@@ -80,59 +79,45 @@ class PyBullet(Environment):
             timestep (float, 0.00416666666): The timestep used by the PyBullet
                 simulator;
             n_intermediate_steps (int): The number of steps between every action
-                taken by the agent. Allows the user
-                to modify, control and access intermediate states.
-            additional_data_spec (list): A list containing the data fields of
-                interest, which should be read from or written to during
-                simulation. The entries are given as the following tuples:
-                (key, name, type) key is a string for later referencing in the
-                "read_data" and "write_data" methods. The name is the name of
-                the object in the XML specification and the type is the
-                ObservationType;
-            collision_groups (list): A list containing groups of geoms for
-                which collisions should be checked during simulation via
-                ``check_collision``. The entries are given as:
-                ``(key, geom_names)``, where key is a string for later
-                referencing in the "check_collision" method, and geom_names is
-                a list of geom names in the XML specification.
+                taken by the agent. Allows the user to modify, control and
+                access intermediate states;
         """
 
+        # Store simulation parameters
         self._timestep = timestep
+        self._n_intermediate_steps = n_intermediate_steps
 
-        # Create the simulation
+        # Create the simulation and viewer
         pybullet.connect(pybullet.DIRECT)
         pybullet.setTimeStep(self._timestep)
+        pybullet.setGravity(0, 0, -9.81)
         pybullet.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-        self._model_id = pybullet.loadURDF(file_name)
-
-        self.n_intermediate_steps = n_intermediate_steps
-        self.viewer = None
+        self._viewer = PyBulletViewer((500, 500), self._timestep * self._n_intermediate_steps)
         self._state = None
+
+        # Load model
+        self._model_ids = list()
+        for file_name in files:
+            self._model_ids.append(pybullet.loadURDF(file_name))
+        self._model_ids += self._custom_load_models()
 
         # Read the actuation spec and build the mapping between actions and ids
         # as well as their limits
         self._joint_map = dict()
-        for id in range(pybullet.getNumJoints(self._model_id)):
-            name = pybullet.getJointInfo(self._model_id, id)[1].decode('UTF-8')
-            self._joint_map[name] = id
+        for model_id in self._model_ids:
+            for joint_id in range(pybullet.getNumJoints(model_id)):
+                name = pybullet.getJointInfo(model_id, joint_id)[1].decode('UTF-8')
+                self._joint_map[name] = (model_id, joint_id)
 
-        if len(actuation_spec) == 0:
-            self.action_indices = [i for i in pybullet.getNumJoints(self._model_id)]
-        else:
-            self.action_indices = []
-            for name in actuation_spec:
-                if name in self._joint_map:
-                    self.action_indices.append(self._joint_map[name])
+        assert(len(actuation_spec) > 0)
+        self._action_data = list()
+        for name, mode in actuation_spec:
+            if name in self._joint_map:
+                data = self._joint_map[name] + (mode,)
+                self._action_data.append(data)
 
-        self._control_mode = control_mode
-
-        low = []
-        high = []
-        for index in self.action_indices:
-            #TODO add limits
-            low.append(-np.inf)
-            high.append(np.inf)
+        low, high = self._compute_action_limits()
         action_space = Box(np.array(low), np.array(high))
 
         # Read the observation spec to build a mapping at every step. It is
@@ -141,34 +126,19 @@ class PyBullet(Environment):
             raise AttributeError("No Environment observations were specified. "
                                  "Add at least one observation to the observation_spec.")
         else:
-            self.observation_map = observation_spec
+            self._observation_map = observation_spec
 
         # We can only specify limits for the joint positions, all other
         # information can be potentially unbounded
-        # TODO fix this
-        low = np.array([-np.inf]*len(self.observation_map))
-        high = np.array([np.inf]*len(self.observation_map))
+        low, high = self._compute_observation_limits()
         observation_space = Box(low, high)
-
-
-        # Pre-process the additional data to allow easier writing and reading
-        # to and from arrays in MuJoCo
-        # self.additional_data = {}
-        # for key, name, ot in additional_data_spec:
-        #     self.additional_data[key] = (name, ot)
-
-        # Pre-process the collision groups for "fast" detection of contacts
-        # self.collision_groups = {}
-        # if self.collision_groups is not None:
-        #     for name, geom_names in collision_groups:
-        #         self.collision_groups[name] = {self.sim.model._geom_name2id[geom_name]
-        #                                        for geom_name in geom_names}
 
         # Finally, we create the MDP information and call the constructor of
         # the parent class
         mdp_info = MDPInfo(observation_space, action_space, gamma, horizon)
         super().__init__(mdp_info)
-        self._viewer = PyBulletViewer((500, 500), self._timestep*self.n_intermediate_steps)
+
+        # Save initial state of the MDP
         self._initial_state = pybullet.saveState()
 
     def seed(self, seed):
@@ -176,7 +146,6 @@ class PyBullet(Environment):
 
     def reset(self, state=None):
         pybullet.restoreState(self._initial_state)
-        #pybullet.resetSimulation()
         self.setup()
         pybullet.stepSimulation()
         self._state = self._create_observation()
@@ -188,19 +157,6 @@ class PyBullet(Environment):
     def stop(self):
         pass
 
-    def _create_observation(self):
-        data_obs = list()
-
-        for name, type in self.observation_map:
-            idx = self._joint_map[name]
-            pos, vel, _, _ = pybullet.getJointState(self._model_id, idx)
-            if type == PyBulletObservationType.JOINT_POS:
-                data_obs.append(pos)
-            elif type == PyBulletObservationType.JOINT_VEL:
-                data_obs.append(vel)
-
-        return np.array(data_obs)
-
     def step(self, action):
         cur_obs = self._state
 
@@ -208,7 +164,7 @@ class PyBullet(Environment):
 
         self._step_init(cur_obs, action)
 
-        for i in range(self.n_intermediate_steps):
+        for i in range(self._n_intermediate_steps):
 
             ctrl_action = self._compute_action(action)
             self._apply_control(ctrl_action)
@@ -227,18 +183,72 @@ class PyBullet(Environment):
 
         return self._state, reward, self.is_absorbing(self._state), {}
 
-    def _apply_control(self, action):
-        if self._control_mode is pybullet.POSITION_CONTROL:
-            kwargs = dict(targetPositions=action)
-        elif self._control_mode is pybullet.VELOCITY_CONTROL:
-            kwargs = dict(targetVelocities=action)
-        elif self._control_mode is pybullet.TORQUE_CONTROL:
-            kwargs = dict(forces=action)
-        else:
-            raise NotImplementedError
+    def _compute_action_limits(self):
+        low = list()
+        high = list()
 
-        pybullet.setJointMotorControlArray(self._model_id, self.action_indices,
-                                           self._control_mode, **kwargs)
+        for model_id, joint_id, mode in self._action_data:
+            joint_info = pybullet.getJointInfo(model_id, joint_id)
+            if mode is pybullet.POSITION_CONTROL:
+                low.append(joint_info[8])
+                high.append(joint_info[9])
+            elif mode is pybullet.VELOCITY_CONTROL:
+                low.append(-joint_info[11])
+                high.append(joint_info[11])
+            elif mode is pybullet.TORQUE_CONTROL:
+                low.append(-joint_info[10])
+                high.append(joint_info[10])
+            else:
+                raise NotImplementedError
+
+        return np.array(low), np.array(high)
+
+    def _compute_observation_limits(self):
+        low = list()
+        high = list()
+
+        for name, type in self._observation_map:
+            model_id, joint_id = self._joint_map[name]
+            joint_info = pybullet.getJointInfo(model_id, joint_id)
+
+            if type is PyBulletObservationType.JOINT_POS:
+                low.append(joint_info[8])
+                high.append(joint_info[9])
+            else:
+                low.append(-np.inf)
+                high.append(np.inf)
+
+        return np.array(low), np.array(high)
+
+    def _create_observation(self):
+        data_obs = list()
+
+        for name, type in self._observation_map:
+            model_id, joint_id = self._joint_map[name]
+            pos, vel, _, _ = pybullet.getJointState(model_id, joint_id)
+            if type is PyBulletObservationType.JOINT_POS:
+                data_obs.append(pos)
+            elif type is PyBulletObservationType.JOINT_VEL:
+                data_obs.append(vel)
+
+        return np.array(data_obs)
+
+    def _apply_control(self, action):
+
+        i = 0
+        for model_id, joint_id, mode in self._action_data:
+            u = action[i]
+            if mode is pybullet.POSITION_CONTROL:
+                kwargs = dict(targetPosition=u)
+            elif mode is pybullet.VELOCITY_CONTROL:
+                kwargs = dict(targetVelocity=u)
+            elif mode is pybullet.TORQUE_CONTROL:
+                kwargs = dict(force=u)
+            else:
+                raise NotImplementedError
+
+            pybullet.setJointMotorControl2(model_id, joint_id, mode, **kwargs)
+            i += 1
 
     def _preprocess_action(self, action):
         """
@@ -301,6 +311,15 @@ class PyBullet(Environment):
         Allows information to be accesed at the end of a step.
         """
         pass
+
+    def _custom_load_models(self):
+        """
+        Allows to custom load a set of objects in the simulation
+
+        Returns:
+            A list with the ids of the loaded objects
+        """
+        return list()
 
     def reward(self, state, action, next_state):
         """
--- mushroom_rl/environments/pybullet_envs/ball_rolling.py
+++ mushroom_rl/environments/pybullet_envs/ball_rolling.py
@@ -7,8 +7,15 @@ from mushroom_rl.environments.pybullet import PyBullet, PyBulletObservationType
 
 class BallRolling(PyBullet):
     def __init__(self):
-        urdf_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "ball_rolling", "darias.urdf")
-        action_spec = ["R_SFE", "R_SAA", "R_HR",  "R_EB",  "R_WR",  "R_WFE", "R_WAA"]
+        robot_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "ball_rolling", "darias.urdf")
+        action_spec = [("R_SFE", pybullet.POSITION_CONTROL),
+                       ("R_SAA", pybullet.POSITION_CONTROL),
+                       ("R_HR", pybullet.POSITION_CONTROL),
+                       ("R_EB", pybullet.POSITION_CONTROL),
+                       ("R_WR", pybullet.POSITION_CONTROL),
+                       ("R_WFE", pybullet.POSITION_CONTROL),
+                       ("R_WAA", pybullet.POSITION_CONTROL)
+                       ]
 
         observation_spec = [("R_SFE", PyBulletObservationType.JOINT_POS),
                             ("R_SFE", PyBulletObservationType.JOINT_VEL),
@@ -25,7 +32,33 @@ class BallRolling(PyBullet):
                             ("R_WAA", PyBulletObservationType.JOINT_POS),
                             ("R_WAA", PyBulletObservationType.JOINT_VEL)
                             ]
-        super().__init__(urdf_path, action_spec, pybullet.POSITION_CONTROL, observation_spec, 0.99, 1000)
+
+        files = [
+            'plane.urdf',
+            robot_path
+        ]
+
+        super().__init__(files, action_spec, pybullet.POSITION_CONTROL, observation_spec, 0.99, 1000)
+
+        plane, robot, table, ball = self._model_ids
+        pybullet.changeDynamics(plane, -1, restitution=1)
+        pybullet.changeDynamics(table, -1, restitution=1)
+        pybullet.changeDynamics(ball, -1, restitution=0.8, rollingFriction=0.01)
+        pybullet.changeDynamics(robot, -1, restitution=1)
+        for i in range(pybullet.getNumJoints(robot)):
+            pybullet.changeDynamics(robot, i, restitution=1)
+
+    def setup(self):
+        plane, robot, table, ball = self._model_ids
+
+        x_start = np.random.rand() * 0.2 + 0.4
+        speed = np.random.rand() * 2 + 2
+        theta = (np.random.rand() * 20 - 10) / 180 * np.pi
+
+        # self.darias.resetJoints(self.initial_joints)
+        pybullet.resetBasePositionAndOrientation(table, [0.5, 0, 0.3], [0, 0, 0, 1])
+        pybullet.resetBasePositionAndOrientation(ball, [x_start, 1, 0.8], [0, 0, 0, 1])
+        pybullet.resetBaseVelocity(ball, [speed * np.sin(theta), -speed * np.cos(theta), 0], [0, 0, 0])
 
     def reward(self, state, action, next_state):
         # R = [-1, -1, -1]
@@ -62,14 +95,50 @@ class BallRolling(PyBullet):
     # def getMagnitude(self, vec):
     #     return np.sqrt(vec[0]**2 + vec[1]**2 + vec[2]**2)
 
+    def _custom_load_models(self):
+
+        table_collision = pybullet.createCollisionShape(pybullet.GEOM_BOX, halfExtents=[0.5, 1.5, 0.3])
+        table_visual = pybullet.createVisualShape(pybullet.GEOM_BOX, halfExtents=[0.5, 1.5, 0.3],
+                                                  rgbaColor=[0.2, 1, 0.5, 1])
+        table = pybullet.createMultiBody(baseMass=10.0,
+                                         baseCollisionShapeIndex=table_collision,
+                                         baseVisualShapeIndex=table_visual,
+                                         basePosition=[0.5, 0, 0.3])
+
+        ball_collision = pybullet.createCollisionShape(pybullet.GEOM_SPHERE, radius=0.2)
+        ball_visual = pybullet.createVisualShape(pybullet.GEOM_SPHERE, radius=0.2, rgbaColor=[0.2, 0.5, 1, 1])
+        ball = pybullet.createMultiBody(baseMass=0.2,
+                                        baseCollisionShapeIndex=ball_collision,
+                                        baseVisualShapeIndex=ball_visual,
+                                        basePosition=[0.5, 1, 0.8])
+
+        return [table, ball]
+
 
 
 if __name__ == '__main__':
-    mdp = BallRolling()
+    from mushroom_rl.core import Core
+    from mushroom_rl.algorithms import Agent
 
+
+    class DummyAgent(Agent):
+        def __init__(self, n_actions):
+            self._n_actions = n_actions
+
+        def draw_action(self, state):
+            return np.random.randn(self._n_actions)
+
+        def episode_start(self):
+            pass
+
+        def fit(self, dataset):
+            pass
+
+
+    mdp = BallRolling()
+    agent = DummyAgent(mdp.info.action_space.shape[0])
+
+    core = Core(agent, mdp)
     mdp.reset()
-    absorbing = False
-    while not absorbing:
-        action = np.random.randn(mdp.info.action_space.shape[0])
-        state, reward, absorbing, _ = mdp.step(action)
-        mdp.render()
+
+    core.evaluate(n_episodes=3, render=True)
