3fa7fc067c43ce182f669fd27eba5c9352e8339c
Carlo <deramo@penrose.ias.informatik.tu-darmstadt.de>
2020-09-25 11:48:40 +0200

    Fixed imports in td __init__.py

--- mushroom_rl/algorithms/value/td/__init__.py
+++ mushroom_rl/algorithms/value/td/__init__.py
@@ -3,7 +3,7 @@ from .sarsa import SARSA
 from .sarsa_lambda import SARSALambda
 from .expected_sarsa import ExpectedSARSA
 from .q_learning import QLearning
-from .q_lambda import QLearningLambda
+from .q_lambda import QLambda
 from .double_q_learning import DoubleQLearning
 from .speedy_q_learning import SpeedyQLearning
 from .r_learning import RLearning
@@ -13,6 +13,6 @@ from .sarsa_lambda_continuous import SARSALambdaContinuous
 from .true_online_sarsa_lambda import TrueOnlineSARSALambda
 
 __all__ = ['SARSA', 'SARSALambda', 'ExpectedSARSA', 'QLearning',
-           'QLearningLambda','DoubleQLearning', 'SpeedyQLearning',
+           'QLambda', 'DoubleQLearning', 'SpeedyQLearning',
            'RLearning', 'WeightedQLearning', 'RQLearning',
            'SARSALambdaContinuous', 'TrueOnlineSARSALambda']
