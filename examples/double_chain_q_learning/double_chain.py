de3d9fa7b8710bca9004c8e6c2959e5c103cd395
boris-il-forte <boris.ilpossente@hotmail.it>
2020-10-29 11:45:34 +0100

    Fixed bugs in example and collect q callback
    - fixed bug in double chain
    - added misisng import in collect_q callback

--- examples/double_chain_q_learning/double_chain.py
+++ examples/double_chain_q_learning/double_chain.py
@@ -1,5 +1,6 @@
 import numpy as np
 from joblib import Parallel, delayed
+from pathlib import Path
 
 from mushroom_rl.algorithms.value import QLearning, DoubleQLearning,\
     WeightedQLearning, SpeedyQLearning
@@ -22,8 +23,9 @@ def experiment(algorithm_class, exp):
     np.random.seed()
 
     # MDP
-    p = np.load('chain_structure/p.npy')
-    rew = np.load('chain_structure/rew.npy')
+    path = Path(__file__).resolve().parent / 'chain_structure'
+    p = np.load(path / 'p.npy')
+    rew = np.load(path / 'rew.npy')
     mdp = FiniteMDP(p, rew, gamma=.9)
 
     # Policy
@@ -36,7 +38,7 @@ def experiment(algorithm_class, exp):
     agent = algorithm_class(mdp.info, pi, **algorithm_params)
 
     # Algorithm
-    collect_Q = CollectQ(agent.approximator)
+    collect_Q = CollectQ(agent.Q)
     callbacks = [collect_Q]
     core = Core(agent, mdp, callbacks)
 
