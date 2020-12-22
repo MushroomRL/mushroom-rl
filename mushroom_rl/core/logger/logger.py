0421cfe39f553a57439272be743cbf62e8d01191
boris-il-forte <boris.ilpossente@hotmail.it>
2020-12-22 15:16:28 +0100

    Minor modifications to logger

--- mushroom_rl/core/logger/logger.py
+++ mushroom_rl/core/logger/logger.py
@@ -21,8 +21,8 @@ class Logger(DataLogger, ConsoleLogger):
                 not specified, a time-stamped directory is created inside
                 a 'log' folder;
             log_console (bool, False): whether to log or not the console output;
-            use_timestamp (bool, False): If true, adds timestamp to the console
-                log file;
+            use_timestamp (bool, False): If true, adds the current timestamp to
+                the folder name;
             append (bool, False): If true, the logger will append the new
                 data logged to the one already existing in the directory;
             seed (int, None): seed for the current run. It can be optionally
@@ -36,15 +36,15 @@ class Logger(DataLogger, ConsoleLogger):
         if results_dir is None:
             results_dir = Path('.', 'logs') / timestamp
         else:
+            if use_timestamp:
+                results_dir += '_' + timestamp
             results_dir = Path(results_dir)
 
         print('Logging in folder: ' + results_dir.name)
         results_dir.mkdir(parents=True, exist_ok=True)
 
         suffix = '' if seed is None else '-' + str(seed)
+        
         DataLogger.__init__(self, results_dir, suffix=suffix, append=append)
-
-        suffix += '-' + timestamp if use_timestamp else ''
-
         ConsoleLogger.__init__(self, results_dir if log_console else None,
                                suffix=suffix, **kwargs)
