b23b6351da39f3d7ae200f1d793990481159a6a0
carloderamo <carlo.deramo@gmail.com>
2021-01-08 16:15:59 +0100

    Minor change for optimization in FQI and BoostedFQI.

--- mushroom_rl/algorithms/value/batch_td/boosted_fqi.py
+++ mushroom_rl/algorithms/value/batch_td/boosted_fqi.py
@@ -31,8 +31,8 @@ class BoostedFQI(FQI):
         super().__init__(mdp_info, policy, approximator, n_iterations, approximator_params, fit_params, quiet)
 
     def fit(self, x):
+        state, action, reward, next_state, absorbing, _ = parse_dataset(x)
         for _ in trange(self._n_iterations, dynamic_ncols=True, disable=self._quiet, leave=False):
-            state, action, reward, next_state, absorbing, _ = parse_dataset(x)
             if self._target is None:
                 self._target = reward
             else:
