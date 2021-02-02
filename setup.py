24c1ef367b9501acf3454a04db351e56cce4f6e8
carloderamo <carlo.deramo@gmail.com>
2021-02-02 17:33:14 +0100

    Small update in setup.py

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ for group_name in extras:
         all_deps += extras[group_name]
 extras['all'] = all_deps
 
-long_description = 'Mushroom is a Python Reinforcement Learning (RL) library' \
+long_description = 'MushroomRL is a Python Reinforcement Learning (RL) library' \
                    ' whose modularity allows to easily use well-known Python' \
                    ' libraries for tensor computation (e.g. PyTorch, Tensorflow)' \
                    ' and RL benchmarks (e.g. OpenAI Gym, PyBullet, Deepmind' \
