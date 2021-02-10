Fix to n-step return computation in Rainbow.

mushroom_rl/algorithms/value/dqn/rainbow.py
@@ -108,22 +108,25 @@ class Rainbow(AbstractDQN):
 
         super().__init__(mdp_info, policy, TorchApproximator, **params)
 
+        self._gamma_r = self.mdp_info.gamma ** np.arange(self._n_steps_return)
+        self._gamma_z = self.mdp_info.gamma ** self._n_steps_return
+
     def fit(self, dataset):
         self._replay_memory.add(dataset)
         if self._replay_memory.initialized:
-            state, action, reward, next_state, absorbing, _ =\
+            state, action, reward, next_state, absorbing, _ = \
                 self._replay_memory.get(self._batch_size(), self._n_steps_return)
             if self._clip_reward:
                 reward = np.clip(reward, -1, 1)
 
             q_next = self.approximator.predict(next_state)
             a_max = np.argmax(q_next, axis=1)
-            gamma = self.mdp_info.gamma ** np.arange(self._n_steps_return + 1) * (1 - absorbing).reshape(-1, 1)
+            gamma_z = self._gamma_z * (1 - absorbing.reshape(-1, 1))
             p_next = self.target_approximator.predict(next_state, a_max,
                                                       get_distribution=True)
-            gamma_r = reward.reshape(self._batch_size.get_value(), -1) * gamma[:, :-1]
-            gamma_z = gamma[:, -1:] * np.expand_dims(self._a_values, 0).repeat(len(gamma), 0)
-            bell_a = (gamma_r.sum(1, keepdims=True) + gamma_z).clip(self._v_min, self._v_max)
+            gamma_r = reward.reshape(len(reward), -1) * self._gamma_r
+            z = gamma_z * np.expand_dims(self._a_values, 0).repeat(len(gamma_z), 0)
+            bell_a = (gamma_r.sum(1, keepdims=True) + z).clip(self._v_min, self._v_max)
 
             b = (bell_a - self._v_min) / self._delta
             l = np.floor(b).astype(np.int)