f4181a232b056e5a4e2af794607fb003945d0db0
sparisi <sparisi@users.noreply.github.com>
2021-07-26 00:45:20 +0200

    Update ddpg_from_pixels.py

--- docs/source/tutorials/code/ddpg_from_pixels.py
+++ docs/source/tutorials/code/ddpg_from_pixels.py
@@ -13,18 +13,18 @@ from mushroom_rl.utils.dataset import compute_J
 
 
 class CriticNetwork(nn.Module):
-    def __init__(self, obs_shape, action_shape, n_features, **kwargs):
+    def __init__(self, input_shape, output_shape, n_features, **kwargs):
         super().__init__()
 
-        n_input = obs_shape[0]
-        n_output = action_shape[0]
+        n_input = input_shape[0]
+        n_output = output_shape[0]
 
         self._h1 = nn.Conv2d(n_input, 32, kernel_size=8, stride=3)
         self._h2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
         self._h3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
-        dummy_obs = torch.zeros(1, *obs_shape)
+        dummy_obs = torch.zeros(1, *input_shape)
         conv_out_size = np.prod(self._h3(self._h2(self._h1(dummy_obs))).shape)
-        self._h4 = nn.Linear(conv_out_size + n_output, self.n_features)
+        self._h4 = nn.Linear(conv_out_size + n_input[4], self.n_features)
         self._h5 = nn.Linear(self.n_features, 1)
 
         nn.init.xavier_uniform_(self._h1.weight,
@@ -42,28 +42,24 @@ class CriticNetwork(nn.Module):
         h = F.relu(self._h1(state.float() / 255.))
         h = F.relu(self._h2(h))
         h = F.relu(self._h3(h))
+        h = torch.cat((h.view(state.shape[0], -1), action.float()), dim=1)
         h = F.relu(self._h4(h.view(state.shape[0], -1)))
         q = self._h5(h)
-
-        state_action = torch.cat((state.float(), action.float()), dim=1)
-        features1 = F.relu(self._h1(state_action))
-        features2 = F.relu(self._h2(features1))
-        q = self._h3(features2)
 
         return torch.squeeze(q)
 
 
 class ActorNetwork(nn.Module):
-    def __init__(self, obs_shape, action_shape, n_features, **kwargs):
+    def __init__(self, input_shape, output_shape, n_features, **kwargs):
         super(ActorNetwork, self).__init__()
 
-        n_input = obs_shape[0]
-        n_output = action_shape[0]
+        n_input = input_shape[0]
+        n_output = output_shape[0]
 
         self._h1 = nn.Conv2d(n_input, 32, kernel_size=8, stride=3)
         self._h2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
         self._h3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
-        dummy_obs = torch.zeros(1, *obs_shape)
+        dummy_obs = torch.zeros(1, *input_shape)
         conv_out_size = np.prod(self._h3(self._h2(self._h1(dummy_obs))).shape)
         self._h4 = nn.Linear(conv_out_size, self.n_features)
         self._h5 = nn.Linear(self.n_features, n_output)
@@ -79,12 +75,10 @@ class ActorNetwork(nn.Module):
         nn.init.xavier_uniform_(self._h5.weight,
                                 gain=nn.init.calculate_gain('linear'))
 
-
     def forward(self, state):
         h = F.relu(self._h1(state.float() / 255.))
         h = F.relu(self._h2(h))
         h = F.relu(self._h3(h))
-        h = torch.cat((h.view(state.shape[0], -1), action.float()), dim=1)
         h = F.relu(self._h4(h.view(state.shape[0], -1)))
         q = self._h5(h)
 
@@ -109,24 +103,23 @@ n_features = 80
 tau = .001
 
 # Approximator
-obs_shape = mdp.info.observation_space.shape
-act_shape = mdp.info.action_space.shape
-
+actor_input_shape = mdp.info.observation_space.shape
 actor_params = dict(network=ActorNetwork,
                     n_features=n_features,
-                    input_shape=obs_shape,
-                    output_shape=act_shape)
+                    input_shape=actor_input_shape,
+                    output_shape=mdp.info.action_space.shape)
 
 actor_optimizer = {'class': optim.Adam,
                    'params': {'lr': 1e-5}}
 
+critic_input_shape = actor_input_shape + mdp.info.action_space.shape
 critic_params = dict(network=CriticNetwork,
                      optimizer={'class': optim.Adam,
                                 'params': {'lr': 1e-3}},
                      loss=F.mse_loss,
                      n_features=n_features,
-                     obs_shape=obs_shape,
-                     act_shape=act_shape)
+                     input_shape=critic_input_shape,
+                     output_shape=(1,))
 
 # Agent
 agent = DDPG(mdp.info, policy_class, policy_params,
