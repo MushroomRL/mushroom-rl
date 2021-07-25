5cbf4283fffc8eacb6509ba592b749f8e2f634ba
sparisi <sparisi@users.noreply.github.com>
2021-07-25 23:52:45 +0200

    Update dm_control_env.py

--- mushroom_rl/environments/dm_control_env.py
+++ mushroom_rl/environments/dm_control_env.py
@@ -19,7 +19,7 @@ class DMControl(Environment):
 
     """
     def __init__(self, domain_name, task_name, horizon=None, gamma=0.99, task_kwargs=None,
-                 dt=.01, width_screen=480, height_screen=480, camera_id=0, 
+                 dt=.01, width_screen=480, height_screen=480, camera_id=0,
                  use_pixels=False, pixels_width=64, pixels_height=64):
         """
         Constructor.
@@ -52,9 +52,15 @@ class DMControl(Environment):
         # Hack to ignore dm_control time limit.
         self.env._step_limit = np.inf
 
+        if use_pixels:
+            self._convert_observation_space = self._convert_observation_space_pixels
+            self._convert_observation = self._convert_observation_pixels
+        else:
+            self._convert_observation_space = self._convert_observation_space_vector
+            self._convert_observation = self._convert_observation_vector
+
         # MDP properties
         action_space = self._convert_action_space(self.env.action_spec())
-        print(self.env.observation_spec())
         observation_space = self._convert_observation_space(self.env.observation_spec())
         mdp_info = MDPInfo(observation_space, action_space, gamma, horizon)
 
@@ -92,7 +98,7 @@ class DMControl(Environment):
         pass
 
     @staticmethod
-    def _convert_observation_space(observation_space):
+    def _convert_observation_space_vector(observation_space):
         observation_shape = 0
         for i in observation_space:
             shape = observation_space[i].shape
@@ -103,6 +109,13 @@ class DMControl(Environment):
 
         return Box(low=-np.inf, high=np.inf, shape=(observation_shape,))
 
+
+    @staticmethod
+    def _convert_observation_space_pixels(observation_space):
+        return Box(
+            low=0., high=255.,
+            shape=(3, observation_space['pixels'][0], observation_space['pixels'][1]))
+
     @staticmethod
     def _convert_action_space(action_space):
         low = action_space.minimum
@@ -111,9 +124,13 @@ class DMControl(Environment):
         return Box(low=np.array(low), high=np.array(high))
 
     @staticmethod
-    def _convert_observation(observation):
+    def _convert_observation_vector(observation):
         obs = list()
         for i in observation:
             obs.append(np.atleast_1d(observation[i]))
 
         return np.concatenate(obs)
+
+    @staticmethod
+    def _convert_observation_pixels(observation):
+        return observation.transpose((2, 0, 1))
