2a2e0ec10e608adc3641a6cb502043529c323ddf
sparisi <sparisi@users.noreply.github.com>
2021-08-03 14:48:54 +0200

    Create mj_env.py

---
+++ mushroom_rl/environments/mj_envs/mj_env.py
@@ -0,0 +1,68 @@
+import warnings
+
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    from mj_envs.hand_manipulation_suite import *
+
+from mushroom_rl.core import Environment, MDPInfo
+from mushroom_rl.utils.spaces import *
+from mushroom_rl.utils.viewer import ImageViewer
+
+
+class MJEnv(Environment):
+    """
+
+    """
+    def __init__(self, env_name, gamma, horizon, **kwargs):
+        """
+        Constructor.
+
+        Args:
+             domain_name (str): name of the environment;
+
+        """
+
+        if env_name == 'HSM-Door-v0':
+            self.env = DoorEnvV0(kwargs)
+        elif env_name == 'HSM-Hammer-v0':
+            self.env = HammerEnvV0(kwargs)
+        else:
+            raise NotImplementedError
+
+        # # get the default horizon
+        # if horizon is None:
+        #     horizon = self.env._step_limit
+        #
+        # # Hack to ignore dm_control time limit.
+        # self.env._step_limit = np.inf
+
+        # MDP properties
+        action_space = self.env.action_space
+        observation_space = self.env.observation_space
+        mdp_info = MDPInfo(observation_space, action_space, gamma, horizon)
+
+        # self._viewer = ImageViewer((width_screen, height_screen), dt)
+        # self._camera_id = camera_id
+
+        super().__init__(mdp_info)
+
+        self._state = None
+
+    def reset(self, state=None):
+        if state is None:
+            return self.env.reset()
+        else:
+            raise NotImplementedError
+
+    def step(self, action):
+        return self.env.step(action)
+
+    def render(self):
+        pass
+        # img = self.env.physics.render(self._viewer.size[1],
+        #                               self._viewer.size[0],
+        #                               self._camera_id)
+        # self._viewer.display(img)
+
+    def stop(self):
+        pass
