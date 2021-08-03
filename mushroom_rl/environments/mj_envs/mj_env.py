8ddfa463f927ecee861a71e8d521ef9133b5df2c
sparisi <sparisi@users.noreply.github.com>
2021-08-03 22:00:34 +0200

    Update mj_env.py

--- mushroom_rl/environments/mj_envs/mj_env.py
+++ mushroom_rl/environments/mj_envs/mj_env.py
@@ -3,6 +3,7 @@ import warnings
 with warnings.catch_warnings():
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     from mj_envs.hand_manipulation_suite import *
+    from mjrl.envs import *
 
 import gym
 from mushroom_rl.core import Environment, MDPInfo
@@ -10,12 +11,29 @@ from mushroom_rl.utils.spaces import *
 # from mushroom_rl.utils.viewer import ImageViewer
 
 
+class MuJoCoPixelObs(gym.ObservationWrapper):
+    def __init__(self, env):
+        gym.ObservationWrapper.__init__(self, env, width, height, camera_name, camera_id=0, depth=False)
+        self.observation_space = Box(low=0., high=255., shape=(3, width, height))
+        self.width = width
+        self.height = height
+        self.camera_name = camera_name
+        self.depth = depth
+        self.camera_id = camera_id
+
+    def observation(self, observation):
+        img = self.sim.render(width=self.width, height=self.height, depth=self.depth
+                              camera_name=self.camera_name, camera_id=self.camera_id)
+        img = img[::-1,:,:]
+        return img.transpose((2, 0, 1))
+
+
 class MJEnv(Environment):
     """
 
     """
     def __init__(self, task_name, horizon=None, gamma=0.99,
-        camera_id=0, use_pixels=False, pixels_width=64, pixels_height=64):
+        use_pixels=False, camera_id=0, pixels_width=64, pixels_height=64):
         """
         Constructor.
 
@@ -23,20 +41,20 @@ class MJEnv(Environment):
              task_name (str): name of the task;
              horizon (int): the horizon;
              gamma (float): the discount factor;
-             camera_id (int, 0): position of camera to render the environment;
              use_pixels (bool, False): if True, pixel observations are used
                 rather than the state vector;
+             camera_id (int, 0): position of camera to render the environment;
              pixels_width (int, 64): width of the pixel observation;
              pixels_height (int, 64): height of the pixel observation;
 
         """
 
         self.env = gym.make(task_name)
-
-        self._camera_id = camera_id
-        self._width = pixels_width
-        self._height = pixels_height
-        self._use_pixels = use_pixels
+        if self.use_pixels:
+            self.env = MuJoCoPixelObs(self.env, width=pixels_width,
+                                                height=pixels_height,
+                                                camera_name='vil_camera',
+                                                camera_id=camera_id)
 
         # # get the default horizon
         # if horizon is None:
@@ -50,9 +68,6 @@ class MJEnv(Environment):
         observation_space = self.env.observation_space
         mdp_info = MDPInfo(observation_space, action_space, gamma, horizon)
 
-        # self._viewer = ImageViewer((width_screen, height_screen), dt)
-        # self._camera_id = camera_id
-
         super().__init__(mdp_info)
 
         self._state = None
@@ -64,11 +79,9 @@ class MJEnv(Environment):
             raise NotImplementedError
 
     def step(self, action):
-        return self.env.step(action)
-
-    def pixels(self):
-        img = self.env.sim.render(width=width, height=height, camera_name='vil_camera', depth=depth)
-        return img[::-1,:,:]
+        obs, reward, done, info = self.env.step(action)
+        self._state = obs
+        return obs, reward, done, info
 
     def render(self):
         pass
