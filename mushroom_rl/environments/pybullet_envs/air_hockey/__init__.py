383a955a2592ccff3caa7f1f0ce205b5c263f3d4
Jonas <jonas.guenster@gmx.de>
2021-11-01 20:18:15 +0100

    registered new envs in __init__.py

--- mushroom_rl/environments/pybullet_envs/air_hockey/__init__.py
+++ mushroom_rl/environments/pybullet_envs/air_hockey/__init__.py
@@ -1,5 +1,10 @@
 from mushroom_rl.environments.pybullet_envs.air_hockey.hit import AirHockeyHit
 from mushroom_rl.environments.pybullet_envs.air_hockey.defend import AirHockeyDefend
+from mushroom_rl.environments.pybullet_envs.air_hockey.prepare import AirHockeyPrepare
+from mushroom_rl.environments.pybullet_envs.air_hockey.repelle import AirHockeyRepelle
+
 
 AirHockeyHit.register()
 AirHockeyDefend.register()
+AirHockeyPrepare.register()
+AirHockeyRepelle.register()
