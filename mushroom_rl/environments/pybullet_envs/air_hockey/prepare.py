76e7068589c1c07fb9551bcff12a291edf19719f
jonas <jonas.guenster@gmx.de>
2021-12-13 14:15:48 +0100

    changed prepare reward to not care about puck_vel

--- mushroom_rl/environments/pybullet_envs/air_hockey/prepare.py
+++ mushroom_rl/environments/pybullet_envs/air_hockey/prepare.py
@@ -39,56 +39,42 @@ class AirHockeyPrepare(AirHockeySingle):
         for i, (model_id, joint_id, _) in enumerate(self._indexer.action_data):
             self.client.resetJointState(model_id, joint_id, self.init_state[i])
 
-        self.got_reward = None
         self.has_hit = False
         self.has_bounce = False
 
 
     def reward(self, state, action, next_state, absorbing):
         puck_pos = self.get_sim_state(next_state, "puck", PyBulletObservationType.BODY_POS)[:2]
-        puck_vel = self.get_sim_state(next_state, "puck", PyBulletObservationType.BODY_LIN_VEL)[:2]
 
-        r_vel = np.exp(-max(np.linalg.norm(puck_vel)**2 - 0.1, 0))
-
-        if self.has_hit and not self.has_bounce:
-            r = self.r_hit + r_vel
-
-        elif self.has_bounce:
+        if self.has_hit:
+            # After hit
             dist_puck_des = np.linalg.norm(puck_pos - self.desired_point)
-            r = 0
-            if dist_puck_des < 0.2:
-                if self.got_reward is None:
-                    self.got_reward = True
+            r = 2 * np.exp(-3 * dist_puck_des) + self.r_hit
 
-                if self.got_reward:
-                    r = 2
-            else:
-                if self.got_reward:
-                    self.got_reward = False
         else:
+            # Before hit
             ee_pos = self.get_sim_state(next_state, "planar_robot_1/link_striker_ee",
                                         PyBulletObservationType.LINK_POS)[:2]
 
-            dist_ee_puck = np.linalg.norm(puck_pos - ee_pos[:2])
+            dist_ee_puck = np.linalg.norm(puck_pos - ee_pos)
             vec_ee_puck = (puck_pos - ee_pos) / dist_ee_puck
 
-            height = 0.51 - 0.03165
+            cos_ang = np.clip(vec_ee_puck @ np.array([0, np.copysign(1, puck_pos[1])]), 0, 1)
 
-            # get to point
-            w = (abs(puck_pos[1]) * self.desired_point[0] + self.desired_point[1] * puck_pos[0] - height * puck_pos[0] -
-                 height * self.desired_point[0]) / (abs(puck_pos[1]) + self.desired_point[1] - 2 * height)
-
-            side_point = np.array([w, np.copysign(height, puck_pos[1])])
-
-            vec_puck_side = (side_point - puck_pos) / np.linalg.norm(side_point - puck_pos)
-
-            cos_ang = np.clip(vec_ee_puck @ vec_puck_side, 0, 1)
-
-            r = np.exp(-3 * (dist_ee_puck - 0.08)) * cos_ang
+            r = np.exp(-8 * (dist_ee_puck - 0.08)) * cos_ang ** 2
             self.r_hit = r
 
         r -= self.action_penalty * np.linalg.norm(action)
         return r
+
+    def is_absorbing(self, state):
+        if super().is_absorbing(state):
+            return True
+        if self.has_hit:
+            puck_pos = self.get_sim_state(self._state, "puck", PyBulletObservationType.BODY_POS)[:2]
+            if puck_pos[0] > 0:
+                return True
+        return self.has_bounce
 
     def _simulation_post_step(self):
         if not self.has_hit:
@@ -104,13 +90,22 @@ class AirHockeyPrepare(AirHockeySingle):
         if not self.has_bounce:
             collision_count = 0
             collision_count += len(self.client.getContactPoints(self._model_map['puck'],
-                                                                self._indexer.link_map['t_left_rim'][0],
+                                                                self._indexer.link_map['t_up_rim_l'][0],
                                                                 -1,
-                                                                self._indexer.link_map['t_left_rim'][1]))
+                                                                self._indexer.link_map['t_up_rim_l'][1]))
             collision_count += len(self.client.getContactPoints(self._model_map['puck'],
-                                                                self._indexer.link_map['t_right_rim'][0],
+                                                                self._indexer.link_map['t_up_rim_r'][0],
                                                                 -1,
-                                                                self._indexer.link_map['t_right_rim'][1]))
+                                                                self._indexer.link_map['t_up_rim_r'][1]))
+
+            collision_count += len(self.client.getContactPoints(self._model_map['puck'],
+                                                                self._indexer.link_map['t_down_rim_l'][0],
+                                                                -1,
+                                                                self._indexer.link_map['t_down_rim_l'][1]))
+            collision_count += len(self.client.getContactPoints(self._model_map['puck'],
+                                                                self._indexer.link_map['t_down_rim_r'][0],
+                                                                -1,
+                                                                self._indexer.link_map['t_down_rim_r'][1]))
 
             if collision_count > 0:
                 self.has_bounce = True
