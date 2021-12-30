05d1ab9d975aeae7196fdaf2490945b13050ef78
jonas <jonas.guenster@gmx.de>
2021-12-30 21:55:24 +0100

    fixed bugs in prepare reward

--- mushroom_rl/environments/pybullet_envs/air_hockey/prepare.py
+++ mushroom_rl/environments/pybullet_envs/air_hockey/prepare.py
@@ -11,7 +11,7 @@ class AirHockeyPrepare(AirHockeySingle):
 
         self.random_init = random_init
         self.action_penalty = action_penalty
-        self.init_state = init_state
+        self.init_strat = init_state
 
         self.desired_point = np.array([-0.6, 0])
 
@@ -62,7 +62,7 @@ class AirHockeyPrepare(AirHockeySingle):
         ee_pos = self.get_sim_state(next_state, "planar_robot_1/link_striker_ee",
                                     PyBulletObservationType.LINK_POS)[:2]
 
-        if self.init_state == "side":
+        if self.init_strat == "side":
             if absorbing and abs(puck_pos[1]) < 0.47:
                 r = 100 * np.exp(-2 * np.linalg.norm(puck_vel))
                 return r
@@ -72,7 +72,6 @@ class AirHockeyPrepare(AirHockeySingle):
                     # After hit
                     dist_puck_des = np.linalg.norm(puck_pos - self.desired_point)
                     r_puck = 2 * np.exp(-3 * dist_puck_des ** 2)
-
                     r_vel_x = max([0, 1 - (10 * (np.exp(abs(puck_vel[0])) - 1))])
 
                     dist_ee_des = np.linalg.norm(ee_pos - self.ee_end_pos)
@@ -99,15 +98,18 @@ class AirHockeyPrepare(AirHockeySingle):
                 return r
 
             if self.has_hit:
-                if puck_pos[0] > -0.9:
+                if puck_pos[0] > -0.9 and abs(puck_pos[1]) < 0.47:
                     # After hit
                     dist_puck_des = np.linalg.norm(puck_pos - self.desired_point)
-                    r_puck = 2 * np.exp(-3 * dist_puck_des ** 2)
+                    sig = 0.2
+
+                    x_ratio = puck_vel[0] / abs(puck_vel[1])
+                    r_puck = 1. / (np.sqrt(2. * np.pi) * sig) * np.exp(-np.power((x_ratio - 0.15) / sig, 2.) / 2)
 
                     dist_ee_des = np.linalg.norm(ee_pos - self.ee_end_pos)
                     r_ee = 0.5 * np.exp(-3 * dist_ee_des)
 
-                    r = r_puck + self.r_hit + r_ee
+                    r = r_puck + self.r_hit + r_ee + 1
                 else:
                     r = 0
 
@@ -117,8 +119,7 @@ class AirHockeyPrepare(AirHockeySingle):
                 dist_ee_puck = np.linalg.norm(puck_pos - ee_pos)
                 vec_ee_puck = (puck_pos - ee_pos) / dist_ee_puck
 
-                cos_ang = np.clip(vec_ee_puck @ np.array([-1, 0]), 0, 1)
-
+                cos_ang = np.clip(vec_ee_puck @ np.array([0.5, np.copysign(0.95, puck_pos[1])]), 0, 1)
                 r = np.exp(-8 * (dist_ee_puck - 0.08)) * cos_ang ** 2
                 self.r_hit = r
 
@@ -128,7 +129,7 @@ class AirHockeyPrepare(AirHockeySingle):
     def is_absorbing(self, state):
         if super().is_absorbing(state):
             return True
-        if self.init_state == "side":
+        if self.init_strat == "side":
             if self.has_hit:
                 puck_pos = self.get_sim_state(self._state, "puck", PyBulletObservationType.BODY_POS)[:2]
                 if puck_pos[0] > 0 or abs(puck_pos[1]) < 0.01:
@@ -137,7 +138,7 @@ class AirHockeyPrepare(AirHockeySingle):
         else:
             if self.has_hit:
                 puck_pos = self.get_sim_state(self._state, "puck", PyBulletObservationType.BODY_POS)[:2]
-                if puck_pos[0] >= -0.6:
+                if puck_pos[0] > 0 or abs(puck_pos[1]) < 0.01:
                     return True
             return False
 
@@ -177,7 +178,8 @@ class AirHockeyPrepare(AirHockeySingle):
 
 
 if __name__ == '__main__':
-    env = AirHockeyPrepare(debug_gui=True, obs_noise=False, obs_delay=False, n_intermediate_steps=4, random_init=True, init_state="bottom")
+    env = AirHockeyPrepare(debug_gui=True, obs_noise=False, obs_delay=False, n_intermediate_steps=4, random_init=True,
+                           init_state="bottom")
 
     R = 0.
     J = 0.
