6015a0a7fbe015b61e897265b2eb8ad051b5dddd
jonas <jonas.guenster@gmx.de>
2022-01-12 16:33:12 +0100

    added double env

--- mushroom_rl/environments/pybullet_envs/air_hockey/__init__.py
+++ mushroom_rl/environments/pybullet_envs/air_hockey/__init__.py
@@ -2,9 +2,13 @@ from mushroom_rl.environments.pybullet_envs.air_hockey.hit import AirHockeyHit
 from mushroom_rl.environments.pybullet_envs.air_hockey.defend import AirHockeyDefend
 from mushroom_rl.environments.pybullet_envs.air_hockey.prepare import AirHockeyPrepare
 from mushroom_rl.environments.pybullet_envs.air_hockey.repelle import AirHockeyRepelle
+from mushroom_rl.environments.pybullet_envs.air_hockey.defend_hit import AirHockeyDefendHit
+from mushroom_rl.environments.pybullet_envs.air_hockey.double import AirHockeyDouble
 
 
 AirHockeyHit.register()
 AirHockeyDefend.register()
 AirHockeyPrepare.register()
 AirHockeyRepelle.register()
+AirHockeyDefendHit.register()
+AirHockeyDouble.register()
--- mushroom_rl/environments/pybullet_envs/air_hockey/double.py
+++ mushroom_rl/environments/pybullet_envs/air_hockey/double.py
@@ -11,113 +11,102 @@ class AirHockeyDouble(AirHockeyBase):
     """
     def __init__(self, gamma=0.99, horizon=120, env_noise=False, obs_noise=False, obs_delay=False,
                  torque_control=True, step_action_function=None, timestep=1 / 240., n_intermediate_steps=1,
-                 debug_gui=False):
+                 debug_gui=False, table_boundary_terminate=False):
         self.init_state = np.array([-0.9273, 0.9273, np.pi / 2, -0.9273, 0.9273, np.pi / 2])
         self.obs_prev = None
         super().__init__(gamma=gamma, horizon=horizon, env_noise=env_noise, n_agents=2, obs_noise=obs_noise,
                          obs_delay=obs_delay, torque_control=torque_control, step_action_function=step_action_function,
-                         timestep=timestep, n_intermediate_steps=n_intermediate_steps, debug_gui=debug_gui)
+                         timestep=timestep, n_intermediate_steps=n_intermediate_steps, debug_gui=debug_gui,
+                         table_boundary_terminate=table_boundary_terminate)
 
         self._client.resetDebugVisualizerCamera(cameraDistance=1.5, cameraYaw=-90.0, cameraPitch=-45.0,
                                                 cameraTargetPosition=[-0.5, 0., 0.])
         self._change_dynamics()
-        self._disable_collision()
 
     def _modify_mdp_info(self, mdp_info):
         """
-        puck PyBulletObservationType.BODY_POS 0
-        puck PyBulletObservationType.BODY_LIN_VEL 7
-        puck PyBulletObservationType.BODY_ANG_VEL 10
-        planar_robot_1/joint_1 PyBulletObservationType.JOINT_POS 13
-        planar_robot_1/joint_2 PyBulletObservationType.JOINT_POS 14
-        planar_robot_1/joint_3 PyBulletObservationType.JOINT_POS 15
-        planar_robot_1/joint_1 PyBulletObservationType.JOINT_VEL 16
-        planar_robot_1/joint_2 PyBulletObservationType.JOINT_VEL 17
-        planar_robot_1/joint_3 PyBulletObservationType.JOINT_VEL 18
-        planar_robot_1/link_striker_ee PyBulletObservationType.LINK_POS 19
-        planar_robot_1/link_striker_ee PyBulletObservationType.LINK_LIN_VEL 26
-        planar_robot_2/joint_1 PyBulletObservationType.JOINT_POS 29
-        planar_robot_2/joint_2 PyBulletObservationType.JOINT_POS 30
-        planar_robot_2/joint_3 PyBulletObservationType.JOINT_POS 31
-        planar_robot_2/joint_1 PyBulletObservationType.JOINT_VEL 32
-        planar_robot_2/joint_2 PyBulletObservationType.JOINT_VEL 33
-        planar_robot_2/joint_3 PyBulletObservationType.JOINT_VEL 34
-        planar_robot_2/link_striker_ee PyBulletObservationType.LINK_POS 35
-        planar_robot_2/link_striker_ee PyBulletObservationType.LINK_LIN_VEL 42
-        45
+        {'planar_robot_1/joint_1': {<PyBulletObservationType.JOINT_POS: 3>: [13],
+                                    <PyBulletObservationType.JOINT_VEL: 4>: [16]},
+         'planar_robot_1/joint_2': {<PyBulletObservationType.JOINT_POS: 3>: [14],
+                                    <PyBulletObservationType.JOINT_VEL: 4>: [17]},
+         'planar_robot_1/joint_3': {<PyBulletObservationType.JOINT_POS: 3>: [15],
+                                    <PyBulletObservationType.JOINT_VEL: 4>: [18]},
+         'planar_robot_1/link_striker_ee': {<PyBulletObservationType.LINK_POS: 5>: [19,
+                                                                                    20,
+                                                                                    21,
+                                                                                    22,
+                                                                                    23,
+                                                                                    24,
+                                                                                    25],
+                                            <PyBulletObservationType.LINK_LIN_VEL: 6>: [26,
+                                                                                        27,
+                                                                                        28]},
+         'planar_robot_2/joint_1': {<PyBulletObservationType.JOINT_POS: 3>: [29],
+                                    <PyBulletObservationType.JOINT_VEL: 4>: [32]},
+         'planar_robot_2/joint_2': {<PyBulletObservationType.JOINT_POS: 3>: [30],
+                                    <PyBulletObservationType.JOINT_VEL: 4>: [33]},
+         'planar_robot_2/joint_3': {<PyBulletObservationType.JOINT_POS: 3>: [31],
+                                    <PyBulletObservationType.JOINT_VEL: 4>: [34]},
+         'planar_robot_2/link_striker_ee': {<PyBulletObservationType.LINK_POS: 5>: [35,
+                                                                                    36,
+                                                                                    37,
+                                                                                    38,
+                                                                                    39,
+                                                                                    40,
+                                                                                    41],
+                                            <PyBulletObservationType.LINK_LIN_VEL: 6>: [42,
+                                                                                        43,
+                                                                                        44]},
+         'puck': {<PyBulletObservationType.BODY_POS: 0>: [0, 1, 2, 3, 4, 5, 6],
+                  <PyBulletObservationType.BODY_LIN_VEL: 1>: [7, 8, 9],
+                  <PyBulletObservationType.BODY_ANG_VEL: 2>: [10, 11, 12]}}
         """
-        obs_idx = [0, 1, 2, 7, 8, 9, 13, 14, 15, 16, 17, 18]
+        obs_idx = [0, 1, 2, 7, 8, 9, 13, 14, 15, 16, 17, 18, 0, 1, 2, 7, 8, 9, 29, 30, 31, 32, 33, 34]
         obs_low = mdp_info.observation_space.low[obs_idx]
         obs_high = mdp_info.observation_space.high[obs_idx]
-        obs_low[0:3] = [-1, -0.5, -np.pi]  # puck_pos x, y, z
+        obs_low[0:3] = [-1, -0.5, -np.pi]
         obs_high[0:3] = [1, 0.5, np.pi]
+        obs_low[12:15] = [-1, -0.5, -np.pi]
+        obs_high[12:15] = [1, 0.5, np.pi]
         observation_space = Box(low=obs_low, high=obs_high)
+
         return MDPInfo(observation_space, mdp_info.action_space, mdp_info.gamma, mdp_info.horizon)
 
-    def _get_secondary_obs(self, state):
-        puck_pose = self.get_sim_state(state, "puck", PyBulletObservationType.BODY_POS)
-        puck_pose_2d = self._puck_2d_in_robot_frame(puck_pose, self.agents[0]['frame'], type='pose')
-
-        robot_pos = np.zeros(3)
-        robot_pos[0] = self.get_sim_state(state, self.agents[0]['name'] + "/joint_1", PyBulletObservationType.JOINT_POS)
-        robot_pos[1] = self.get_sim_state(state, self.agents[0]['name'] + "/joint_2", PyBulletObservationType.JOINT_POS)
-        robot_pos[2] = self.get_sim_state(state, self.agents[0]['name'] + "/joint_3", PyBulletObservationType.JOINT_POS)
-
-        if self.obs_noise:
-            puck_pose_2d[:2] += np.random.randn(2) * 0.001
-            puck_pose_2d[2] += np.random.randn(1) * 0.001
-
-        puck_lin_vel = self.get_sim_state(state, "puck", PyBulletObservationType.BODY_LIN_VEL)
-        puck_ang_vel = self.get_sim_state(state, "puck", PyBulletObservationType.BODY_ANG_VEL)
-        puck_vel_2d = self._puck_2d_in_robot_frame(np.concatenate([puck_lin_vel, puck_ang_vel]),
-                                                   self.agents[0]['frame'], type='vel')
-        robot_vel = np.zeros(3)
-        robot_vel[0] = self.get_sim_state(state, self.agents[0]['name'] + "/joint_1",
-                                          PyBulletObservationType.JOINT_VEL)
-        robot_vel[1] = self.get_sim_state(state, self.agents[0]['name'] + "/joint_2",
-                                          PyBulletObservationType.JOINT_VEL)
-        robot_vel[2] = self.get_sim_state(state, self.agents[0]['name'] + "/joint_3",
-                                          PyBulletObservationType.JOINT_VEL)
-
-        if self.obs_delay:
-            alpha = 0.5
-            puck_vel_2d = alpha * puck_vel_2d + (1 - alpha) * self.obs_prev[3:6]
-            robot_vel = alpha * robot_vel + (1 - alpha) * self.obs_prev[9:12]
-
-        self.obs_prev = np.concatenate([puck_pose_2d, puck_vel_2d, robot_pos, robot_vel])
-        return self.obs_prev
 
     def _create_observation(self, state):
         puck_pose = self.get_sim_state(state, "puck", PyBulletObservationType.BODY_POS)
-        puck_pose_2d = self._puck_2d_in_robot_frame(puck_pose, self.agents[0]['frame'], type='pose')
+        obs = np.zeros(24)
+        for i in range(2):
+            puck_pose_2d = self._puck_2d_in_robot_frame(puck_pose, self.agents[i]['frame'], type='pose')
 
-        robot_pos = np.zeros(3)
-        robot_pos[0] = self.get_sim_state(state, self.agents[0]['name'] + "/joint_1", PyBulletObservationType.JOINT_POS)
-        robot_pos[1] = self.get_sim_state(state, self.agents[0]['name'] + "/joint_2", PyBulletObservationType.JOINT_POS)
-        robot_pos[2] = self.get_sim_state(state, self.agents[0]['name'] + "/joint_3", PyBulletObservationType.JOINT_POS)
+            robot_pos = np.zeros(3)
+            robot_pos[0] = self.get_sim_state(state, self.agents[i]['name'] + "/joint_1", PyBulletObservationType.JOINT_POS)
+            robot_pos[1] = self.get_sim_state(state, self.agents[i]['name'] + "/joint_2", PyBulletObservationType.JOINT_POS)
+            robot_pos[2] = self.get_sim_state(state, self.agents[i]['name'] + "/joint_3", PyBulletObservationType.JOINT_POS)
 
-        if self.obs_noise:
-            puck_pose_2d[:2] += np.random.randn(2) * 0.001
-            puck_pose_2d[2] += np.random.randn(1) * 0.001
+            if self.obs_noise:
+                puck_pose_2d[:2] += np.random.randn(2) * 0.001
+                puck_pose_2d[2] += np.random.randn(1) * 0.001
 
-        puck_lin_vel = self.get_sim_state(state, "puck", PyBulletObservationType.BODY_LIN_VEL)
-        puck_ang_vel = self.get_sim_state(state, "puck", PyBulletObservationType.BODY_ANG_VEL)
-        puck_vel_2d = self._puck_2d_in_robot_frame(np.concatenate([puck_lin_vel, puck_ang_vel]),
-                                                   self.agents[0]['frame'], type='vel')
-        robot_vel = np.zeros(3)
-        robot_vel[0] = self.get_sim_state(state, self.agents[0]['name'] + "/joint_1",
-                                          PyBulletObservationType.JOINT_VEL)
-        robot_vel[1] = self.get_sim_state(state, self.agents[0]['name'] + "/joint_2",
-                                          PyBulletObservationType.JOINT_VEL)
-        robot_vel[2] = self.get_sim_state(state, self.agents[0]['name'] + "/joint_3",
-                                          PyBulletObservationType.JOINT_VEL)
+            puck_lin_vel = self.get_sim_state(state, "puck", PyBulletObservationType.BODY_LIN_VEL)
+            puck_ang_vel = self.get_sim_state(state, "puck", PyBulletObservationType.BODY_ANG_VEL)
+            puck_vel_2d = self._puck_2d_in_robot_frame(np.concatenate([puck_lin_vel, puck_ang_vel]),
+                                                       self.agents[i]['frame'], type='vel')
+            robot_vel = np.zeros(3)
+            robot_vel[0] = self.get_sim_state(state, self.agents[i]['name'] + "/joint_1",
+                                              PyBulletObservationType.JOINT_VEL)
+            robot_vel[1] = self.get_sim_state(state, self.agents[i]['name'] + "/joint_2",
+                                              PyBulletObservationType.JOINT_VEL)
+            robot_vel[2] = self.get_sim_state(state, self.agents[i]['name'] + "/joint_3",
+                                              PyBulletObservationType.JOINT_VEL)
 
-        if self.obs_delay:
-            alpha = 0.5
-            puck_vel_2d = alpha * puck_vel_2d + (1 - alpha) * self.obs_prev[3:6]
-            robot_vel = alpha * robot_vel + (1 - alpha) * self.obs_prev[9:12]
+            if self.obs_delay:
+                alpha = 0.5
+                puck_vel_2d = alpha * puck_vel_2d + (1 - alpha) * self.obs_prev[i][3:6]
+                robot_vel = alpha * robot_vel + (1 - alpha) * self.obs_prev[i][9:12]
 
-        self.obs_prev = np.concatenate([puck_pose_2d, puck_vel_2d, robot_pos, robot_vel])
+            obs[i * 12:(i+1) * 12] = np.concatenate([puck_pose_2d, puck_vel_2d, robot_pos, robot_vel])
+        self.obs_prev = obs
         return self.obs_prev
 
     def _puck_2d_in_robot_frame(self, puck_in, robot_frame, type='pose'):
@@ -139,28 +128,3 @@ class AirHockeyDouble(AirHockeyBase):
         for i in range(5):
             self.client.changeDynamics(self._model_map['planar_robot_1'], i, linearDamping=0., angularDamping=0.)
             self.client.changeDynamics(self._model_map['planar_robot_2'], i, linearDamping=0., angularDamping=0.)
-
-    def _disable_collision(self):
-        iiwa_links = ['planar_robot_1/link_1', 'planar_robot_1/link_2', 'planar_robot_1/link_3',
-                      'planar_robot_1/link_striker_hand', 'planar_robot_1/link_striker_ee',
-                      'planar_robot_2/link_1', 'planar_robot_2/link_2', 'planar_robot_2/link_3',
-                      'planar_robot_2/link_striker_hand', 'planar_robot_2/link_striker_ee'
-                      ]
-        table_rims = ['t_down_rim_l', 't_down_rim_r', 't_up_rim_r', 't_up_rim_l',
-                      't_left_rim', 't_right_rim', 't_base', 't_up_rim_top', 't_down_rim_top', 't_base']
-        for iiwa_l in iiwa_links:
-            for table_r in table_rims:
-                self.client.setCollisionFilterPair(self._indexer.link_map[iiwa_l][0],
-                                                   self._indexer.link_map[table_r][0],
-                                                   self._indexer.link_map[iiwa_l][1],
-                                                   self._indexer.link_map[table_r][1], 0)
-
-        self.client.setCollisionFilterPair(self._model_map['puck'], self._indexer.link_map['t_down_rim_top'][0],
-                                           -1, self._indexer.link_map['t_down_rim_top'][1], 0)
-        self.client.setCollisionFilterPair(self._model_map['puck'], self._indexer.link_map['t_up_rim_top'][0],
-                                           -1, self._indexer.link_map['t_up_rim_top'][1], 0)
-
-if __name__ == "__main__":
-    env = AirHockeyDouble(debug_gui=True)
-    while True:
-        ...
