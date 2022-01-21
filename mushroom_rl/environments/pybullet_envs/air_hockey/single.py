8dbc49158e28e5950096da0b4f03786b3bee7fad
jonas <jonas.guenster@gmx.de>
2022-01-21 13:37:26 +0100

    removed ang puck pos from observation

--- mushroom_rl/environments/pybullet_envs/air_hockey/single.py
+++ mushroom_rl/environments/pybullet_envs/air_hockey/single.py
@@ -25,11 +25,11 @@ class AirHockeySingle(AirHockeyBase):
         self._disable_collision()
 
     def _modify_mdp_info(self, mdp_info):
-        obs_idx = [0, 1, 2, 7, 8, 9, 13, 14, 15, 16, 17, 18]
+        obs_idx = [0, 1, 7, 8, 9, 13, 14, 15, 16, 17, 18]
         obs_low = mdp_info.observation_space.low[obs_idx]
         obs_high = mdp_info.observation_space.high[obs_idx]
-        obs_low[0:3] = [-1, -0.5, -np.pi]
-        obs_high[0:3] = [1, 0.5, np.pi]
+        obs_low[0:2] = [-1, -0.5]
+        obs_high[0:2] = [1, 0.5]
         observation_space = Box(low=obs_low, high=obs_high)
         return MDPInfo(observation_space, mdp_info.action_space, mdp_info.gamma, mdp_info.horizon)
 
@@ -60,8 +60,8 @@ class AirHockeySingle(AirHockeyBase):
 
         if self.obs_delay:
             alpha = 0.5
-            puck_vel_2d = alpha * puck_vel_2d + (1 - alpha) * self.obs_prev[3:6]
-            robot_vel = alpha * robot_vel + (1 - alpha) * self.obs_prev[9:12]
+            puck_vel_2d = alpha * puck_vel_2d + (1 - alpha) * self.obs_prev[2:6]
+            robot_vel = alpha * robot_vel + (1 - alpha) * self.obs_prev[8:11]
 
         self.obs_prev = np.concatenate([puck_pose_2d, puck_vel_2d, robot_pos, robot_vel])
         return self.obs_prev
@@ -73,9 +73,8 @@ class AirHockeySingle(AirHockeyBase):
 
             frame_target = transformations.inverse_matrix(robot_frame) @ puck_frame
             puck_translate = transformations.translation_from_matrix(frame_target)
-            _, _, puck_euler_yaw = transformations.euler_from_matrix(frame_target)
 
-            return np.concatenate([puck_translate[:2], [puck_euler_yaw]])
+            return puck_translate[:2]
         if type == 'vel':
             rot_mat = robot_frame[:3, :3]
             vec_lin = rot_mat.T @ puck_in[:3]
