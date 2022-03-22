4e56f5ee444dfacfcc5f1c4e846192441e6b8969
jonas <jonas.guenster@gmx.de>
2022-03-22 08:34:19 +0100

    addd puck avoidance

--- mushroom_rl/environments/pybullet_envs/air_hockey/return_init.py
+++ mushroom_rl/environments/pybullet_envs/air_hockey/return_init.py
@@ -24,9 +24,13 @@ class AirHockeyReturn(AirHockeySingle):
             action_penalty(float, 1e-3): The penalty of the action on the reward at each time step
         """
         self.action_penalty = action_penalty
-        self.spawn_range = np.array([[-0.92, -0.3], [-0.47, 0.47]])
+        self.ee_spawn_range = np.array([[-0.94, -0.3], [-0.47, 0.47]])
+
+        self.puck_spawn_range = np.array([[-0.94, 0.94], [-0.47, 0.47]])
 
         self.desired_conf = np.array([-0.9273, 0.9273, np.pi / 2])
+
+        self.has_hit = False
 
         super().__init__(gamma=gamma, horizon=horizon, timestep=timestep, n_intermediate_steps=n_intermediate_steps,
                          debug_gui=debug_gui, env_noise=env_noise, obs_noise=obs_noise, obs_delay=obs_delay,
@@ -34,12 +38,19 @@ class AirHockeyReturn(AirHockeySingle):
 
 
     def setup(self, state):
-        self.puck_pos = [0, 0]
+        self.puck_pos = np.random.rand(2) * (self.puck_spawn_range[:, 1] - self.puck_spawn_range[:, 0]) + self.puck_spawn_range[:, 0]
 
         puck_pos = np.concatenate([self.puck_pos, [-0.189]])
         self.client.resetBasePositionAndOrientation(self._model_map['puck'], puck_pos, [0, 0, 0, 1.0])
 
-        self.spawn_pos = np.random.rand(2) * (self.spawn_range[:, 1] - self.spawn_range[:, 0]) + self.spawn_range[:, 0]
+        puck_lin_vel = np.random.uniform(-1, 1, 3)
+        puck_lin_vel[2] = 0.0
+        puck_ang_vel = np.random.uniform(-1, 1, 3)
+        puck_ang_vel[:2] = 0.0
+
+        self.client.resetBaseVelocity(self._model_map['puck'], puck_lin_vel, puck_ang_vel)
+
+        self.spawn_pos = np.random.rand(2) * (self.ee_spawn_range[:, 1] - self.ee_spawn_range[:, 0]) + self.ee_spawn_range[:, 0]
 
         robot_id, joint_id = self._indexer.link_map['planar_robot_1/link_striker_ee']
         self.init_state = self.client.calculateInverseKinematics(robot_id, joint_id, np.concatenate([self.spawn_pos, [-0.189]]))
@@ -47,16 +58,29 @@ class AirHockeyReturn(AirHockeySingle):
         for i, (model_id, joint_id, _) in enumerate(self._indexer.action_data):
             self.client.resetJointState(model_id, joint_id, self.init_state[i])
 
+        self.has_hit = False
+
 
     def reward(self, state, action, next_state, absorbing):
         robot_conf = [self.get_sim_state(next_state, 'planar_robot_1/joint_' + str(i), PyBulletObservationType.JOINT_POS)[0]
                       for i in range(1, 4)]
 
-        # print(np.array(robot_conf) - self.desired_conf)
-        # print(np.array(robot_conf), self.desired_conf)
-        r = -np.linalg.norm(self.desired_conf - robot_conf)
+        r = -2
+        if not self.has_hit:
+            r = -np.linalg.norm(self.desired_conf - robot_conf)
         r -= self.action_penalty * np.linalg.norm(action)
         return r
+
+    def _simulation_post_step(self):
+        if not self.has_hit:
+            collision_count = len(self.client.getContactPoints(self._model_map['puck'],
+                                                               self._indexer.link_map['planar_robot_1/'
+                                                                                      'link_striker_ee'][0],
+                                                               -1,
+                                                               self._indexer.link_map['planar_robot_1/'
+                                                                                      'link_striker_ee'][1]))
+            if collision_count > 0:
+                self.has_hit = True
 
 
 
