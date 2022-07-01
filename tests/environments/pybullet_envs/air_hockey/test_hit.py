10e8193d3574b025bb77f452aecc048bd10e8d75
jonas <jonas.guenster@gmx.de>
2022-07-01 13:50:25 +0200

    add test for air hockey

---
+++ tests/environments/pybullet_envs/air_hockey/test_hit.py
@@ -0,0 +1,20 @@
+try:
+    from mushroom_rl.environments.pybullet_envs.air_hockey import AirHockeyHit
+    import numpy as np
+    obs = []
+    mdp = AirHockeyHit()
+    mdp.reset()
+    action = np.array([1] * 3)
+
+    for _ in range(20):
+        observation, _, _, _ = mdp.step(action)
+        assert len(observation) == len(mdp._mdp_info.observation_space.low)
+        assert len(observation) == len(mdp._mdp_info.observation_space.high)
+        obs.append(observation)
+
+    obs_test = np.load("hit_data.npy")
+
+    assert np.allclose(obs, obs_test)
+
+except ImportError:
+    pass
