fix issue #99 by adding eps instead of delta for the numerical stability of arange function

mushroom_rl/algorithms/value/dqn/categorical_dqn.py
@@ -6,6 +6,7 @@ import torch.nn.functional as F
 from mushroom_rl.algorithms.value.dqn import AbstractDQN
 from mushroom_rl.approximators.parametric.torch_approximator import *
 
+eps = torch.finfo(torch.float32).eps
 
 def categorical_loss(input, target, reduction='sum'):
     input = input.clamp(1e-5)
@@ -33,7 +34,7 @@ class CategoricalNetwork(nn.Module):
         self._v_max = v_max
 
         delta = (self._v_max - self._v_min) / (self._n_atoms - 1)
-        self._a_values = torch.arange(self._v_min, self._v_max + delta, delta)
+        self._a_values = torch.arange(self._v_min, self._v_max + eps, delta)
         if use_cuda:
             self._a_values = self._a_values.cuda()
 
@@ -100,7 +101,7 @@ class CategoricalDQN(AbstractDQN):
         self._v_min = v_min
         self._v_max = v_max
         self._delta = (v_max - v_min) / (n_atoms - 1)
-        self._a_values = np.arange(v_min, v_max + self._delta, self._delta)
+        self._a_values = np.arange(v_min, v_max + eps, self._delta)
 
         self._add_save_attr(
             _n_atoms='primitive',

mushroom_rl/algorithms/value/dqn/rainbow.py
@@ -9,6 +9,7 @@ from mushroom_rl.algorithms.value.dqn.noisy_dqn import NoisyNetwork
 from mushroom_rl.approximators.parametric.torch_approximator import *
 from mushroom_rl.utils.replay_memory import PrioritizedReplayMemory
 
+eps = torch.finfo(torch.float32).eps
 
 class RainbowNetwork(nn.Module):
     def __init__(self, input_shape, output_shape, features_network, n_atoms,
@@ -23,7 +24,7 @@ class RainbowNetwork(nn.Module):
         self._v_max = v_max
 
         delta = (self._v_max - self._v_min) / (self._n_atoms - 1)
-        self._a_values = torch.arange(self._v_min, self._v_max + delta, delta)
+        self._a_values = torch.arange(self._v_min, self._v_max + eps, delta)
         if use_cuda:
             self._a_values = self._a_values.cuda()
 
@@ -97,7 +98,7 @@ class Rainbow(AbstractDQN):
         self._v_min = v_min
         self._v_max = v_max
         self._delta = (v_max - v_min) / (n_atoms - 1)
-        self._a_values = np.arange(v_min, v_max + self._delta, self._delta)
+        self._a_values = np.arange(v_min, v_max + eps, self._delta)
         self._n_steps_return = n_steps_return
         self._sigma_coeff = sigma_coeff
 