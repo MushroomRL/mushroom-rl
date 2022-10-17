48612ef46e0068d833401370d4ec948a93d7787a
boris-il-forte <boris.ilpossente@hotmail.it>
2022-10-17 13:09:20 +0200

    Updated requirements
    - fixed typo in setup.py
    - fixed rtd_requirements

--- setup.py
+++ setup.py
@@ -41,9 +41,11 @@ extras = {
 
 all_deps = []
 for group_name in extras:
-    if group_name not in ['mujoco', 'bullet' 'plots']:
+    if group_name not in ['mujoco', 'bullet', 'plots']:
         all_deps += extras[group_name]
 extras['all'] = all_deps
+
+print(extras['all'])
 
 long_description = 'MushroomRL is a Python Reinforcement Learning (RL) library' \
                    ' whose modularity allows to easily use well-known Python' \
