3d2ad241896848b93f4d1e1cd5f53db770678e9a
boris-il-forte <boris.ilpossente@hotmail.it>
2023-05-08 14:34:19 +0200

    Work on tensor fetures
    - manir basis tensor features refactoring
    - Added Von Mises features
    - Added generic class for basis features

--- mushroom_rl/features/tensors/__init__.py
+++ mushroom_rl/features/tensors/__init__.py
@@ -1,5 +1,5 @@
-from .gaussian_tensor import GaussianRBFTensor
+from .basis_tensor import GenericBasisTensor, GaussianRBFTensor, VonMisesBFTensor
 from .constant_tensor import ConstantTensor
 from .random_fourier_tensor import RandomFourierBasis
 
-__all_ = ['GaussianRBFTensor', 'ConstantTensor', 'RandomFourierBasis']
+__all_ = ['GenericBasisTensor', 'GaussianRBFTensor',  'VonMisesBFTensor', 'ConstantTensor', 'RandomFourierBasis']
