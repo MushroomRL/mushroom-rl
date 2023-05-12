fcfe60b730fa60b408ea8df109e8e4f7d232268c
Jonas Günster <jonas.guenster@gmx.de>
2023-05-12 18:12:13 +0200

    added custom render callback in mujoco viewer (#125)
    * added custom render callback in Mujoco viewer

--- mushroom_rl/utils/mujoco/viewer.py
+++ mushroom_rl/utils/mujoco/viewer.py
@@ -13,7 +13,7 @@ class MujocoGlfwViewer:
         c: Turn contact force and constraint visualisation on / off
         t: Make models transparent
     """
-    def __init__(self, model, dt, width=1920, height=1080, start_paused=False):
+    def __init__(self, model, dt, width=1920, height=1080, start_paused=False, custom_render_callback=None):
         self.button_left = False
         self.button_right = False
         self.button_middle = False
@@ -53,6 +53,8 @@ class MujocoGlfwViewer:
         self._context = mujoco.MjrContext(model, mujoco.mjtFontScale(100))
 
         self.rgb_buffer = np.empty((width, height, 3), dtype=np.uint8)
+
+        self.custom_render_callback = custom_render_callback
 
     def mouse_button(self, window, button, act, mods):
         self.button_left = glfw.get_mouse_button(self._window, glfw.MOUSE_BUTTON_LEFT) == glfw.PRESS
@@ -114,7 +116,11 @@ class MujocoGlfwViewer:
                                    self._scene)
 
             self._viewport.width, self._viewport.height = glfw.get_window_size(self._window)
+
             mujoco.mjr_render(self._viewport, self._scene, self._context)
+
+            if self.custom_render_callback is not None:
+                self.custom_render_callback(self._viewport, self._context)
 
             glfw.swap_buffers(self._window)
             glfw.poll_events()
@@ -143,5 +149,4 @@ class MujocoGlfwViewer:
 
     def stop(self):
         glfw.destroy_window(self._window)
-
 
