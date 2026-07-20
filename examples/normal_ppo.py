376fbd364456c62471543766edb9ff4b3db2aaca
rm-fk <re4747gi-s@student.lu.se>
2026-07-20 12:45:48 +0700

    change hyperparameters

--- examples/normal_ppo.py
+++ examples/normal_ppo.py
@@ -211,10 +211,13 @@ def experiment(
 if __name__ == "__main__":
     experiment(
         env_class=AntWarp,
-        num_envs=8192,
-        n_epochs=50,
-        n_steps=300000,
-        n_steps_per_fit=30000,
+        num_envs=8000,  # --> make full number, i.e 8000?
+        n_epochs=25,  # --> half it
+        n_steps=600000,  # --> double the n_steps
+        n_steps_per_fit=64000,  # num_envs * 8
         n_episodes_test=10,
         save_agent=False,
     )
+    # Problem 1: we have diff performance with diff horizon, and worse performance when increasing num_envs.
+    # Problem 2: GPU utilization. Two reasons: the env could be too easy thus the memory is much more than what we need to do computation in the env, so scale won't help. Then the algorithm computation but it shouldn't be too much of an issue in the evaluation. Third reason: could be that Warp kernels are not efficient enough. I should have that step vectorized,
+    # Reason: we have basically different horizons at every fit. If we use the same horizon, we should see that with more env we either learn faster or got better R. We're seeing the opposite now because we're using less data. Keep batch size consistent and same.
