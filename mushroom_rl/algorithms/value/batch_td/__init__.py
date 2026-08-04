58aac4578ecaf8ba46df13528c13330494fbe62a
davide-tateo90 <davide.tateo90@gmail.com>
2026-08-04 15:29:48 +0200

    Linting algorithm folder

--- mushroom_rl/algorithms/value/batch_td/__init__.py
+++ mushroom_rl/algorithms/value/batch_td/__init__.py
@@ -4,4 +4,4 @@ from .double_fqi import DoubleFQI
 from .boosted_fqi import BoostedFQI
 from .lspi import LSPI
 
-__all__ = ['FQI', 'DoubleFQI', 'BoostedFQI', 'LSPI']
+__all__ = ['BatchTD', 'FQI', 'DoubleFQI', 'BoostedFQI', 'LSPI']
