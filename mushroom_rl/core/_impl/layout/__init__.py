182435a372652b2c9dcd4aaec0a7a0d48ebf98f2
davide-tateo90 <davide.tateo90@gmail.com>
2026-09-26 19:53:20 +0200

    LinkedRingLayout moved to own file

--- mushroom_rl/core/_impl/layout/__init__.py
+++ mushroom_rl/core/_impl/layout/__init__.py
@@ -1,7 +1,8 @@
 from .episode_layout import EpisodeLayout
 from .stream_layout import StreamLayout
 from .coded_layout import CodedLayout
-from .ring_layout import RingLayout, LinkedRingLayout
+from .ring_layout import RingLayout
+from .linked_ring_layout import LinkedRingLayout
 
 __all__ = [
     "EpisodeLayout",
